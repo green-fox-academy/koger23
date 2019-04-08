ea19d26e061740795331be0194f0d57ecb65f83e
Gergely Kocsis <koger23@gmail.com>
2019-04-08 14:18:26 +0200

    Bubble finished

---
+++ week-02/day-01/Bubble.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Bubble {
+
+  public static void main(String[] args) {
+
+    //  Create a function that takes a list of numbers as parameter
+    //  Returns a list where the elements are sorted in ascending numerical order
+    //  Make a second boolean parameter, if it's `true` sort that list descending
+
+    //  Example:
+    //    System.out.println(bubble(new int[] {34, 12, 24, 9, 5}));
+    //  should print [5, 9, 12, 24, 34]
+    System.out.println(bubble(new int[] {34, 12, 24, 9, 5}, false));
+    System.out.println(bubble(new int[] {34, 12, 24, 9, 5}, true));
+    //  should print [34, 24, 12, 9, 5]
+
+  }
+
+  private static String bubble(int[] numList, boolean order) {
+
+    Integer[] resultList = new Integer[numList.length];
+
+    for (int i = 0; i < numList.length; i++){
+
+      resultList[i] = numList[i];
+    }
+
+    if (order){
+      Arrays.sort(resultList, Comparator.reverseOrder());
+    } else {
+      Arrays.sort(resultList);
+    }
+
+
+    return Arrays.toString(resultList);
+  }
+
+}
