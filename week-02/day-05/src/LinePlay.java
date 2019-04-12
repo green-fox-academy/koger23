88419901a65f404c9cd983e1b57f37a55a3bc635
Gergely Kocsis <koger23@gmail.com>
2019-04-12 10:09:09 +0200

    feat(LinePlay): base structure made

--- week-02/day-05/src/LinePlay.java
+++ week-02/day-05/src/LinePlay.java
@@ -1,2 +1,43 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+
 public class LinePlay {
+
+  final static int WIDTH = 320;
+  final static int HEIGHT = 320;
+
+
+  public static void mainDraw(Graphics graphics) {
+
+    drawThemAll(WIDTH, HEIGHT, graphics);
+  }
+
+  public static void drawThemAll(int x, int y, Graphics graphics){
+
+    
+
+  }
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Line Play");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
 }
