a7f93d10ed07921a298f3cc4dd8ee3edc383fa75
Gergely Kocsis <koger23@gmail.com>
2019-04-12 11:26:56 +0200

    feat(EnvelopeStar): dtawThemAll method draws a quarter.

--- week-02/day-05/src/EnvelopeStar.java
+++ week-02/day-05/src/EnvelopeStar.java
@@ -10,6 +10,7 @@ public class EnvelopeStar {
   final static int WIDTH = 320;
   final static int HEIGHT = 320;
   final static Color greenColor = new Color(0, 180, 50);
+  final static int STEP = WIDTH / 8;
 
 
   public static void mainDraw(Graphics graphics) {
@@ -19,6 +20,17 @@ public class EnvelopeStar {
   }
 
   public static void drawThemAll(int x, int y, Graphics graphics){
+
+    graphics.drawLine( x / 2, 0, x / 2 + STEP, y / 2);
+
+    if (x <= WIDTH){
+
+      graphics.drawLine( x / 2, 0 + 1 * STEP, x / 2 + 2 * STEP, y / 2);
+      graphics.drawLine( x / 2, 0 + 2 * STEP, x / 2 + 3 * STEP, y / 2);
+      graphics.drawLine( x / 2, 0 + 3 * STEP, x / 2 + 4 * STEP, y / 2);
+
+    }
+
 
 
   }
