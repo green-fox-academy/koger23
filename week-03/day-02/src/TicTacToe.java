23eb643736624e5e5dd9a13bd64eaede6bb93087
Gergely Kocsis <koger23@gmail.com>
2019-04-16 12:37:56 +0200

    feat(TicTacToe): ticTacResult method - reading into a matrix works

--- week-03/day-02/src/TicTacToe.java
+++ week-03/day-02/src/TicTacToe.java
@@ -1,3 +1,10 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 public class TicTacToe {
 
   public static void main(String[] args) {
@@ -18,6 +25,32 @@ public class TicTacToe {
 
   public static String ticTacResult(String path){
 
+    Path src = Paths.get(path);
+
+    try {
+
+      List<String> rows = Files.readAllLines(src);
+
+      String[][] matrix = new String[3][3];
+
+      // populate matrix
+      for (int i = 0; i < 3; i++){
+
+        for (int j = 0; j < 3; j++){
+
+          matrix[i][j] = rows.get(i).substring(i, i+1);
+        }
+      }
+
+      for (String row[] : matrix){
+
+        System.out.println(Arrays.toString(row));
+
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
 
     return "s";
   }
