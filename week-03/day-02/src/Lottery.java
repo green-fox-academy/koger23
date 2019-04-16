cec98d8dbabc26deaf2229b5330d232b941aa46f
Gergely Kocsis <koger23@gmail.com>
2019-04-16 15:34:34 +0200

    feat(Lottery): getting occurence of numbers works

--- week-03/day-02/src/Lottery.java
+++ week-03/day-02/src/Lottery.java
@@ -1,3 +1,9 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Lottery {
@@ -5,10 +11,48 @@ public class Lottery {
   public static void main(String[] args) {
     // Create a method that find the 5 most common lottery numbers in lottery.csv
 
-    getMostCommonNums();
+    getMostCommonNums("assets/lottery.csv");
   }
 
-  public static void getMostCommonNums(){
+  public static void getMostCommonNums(String path){
+
+    Path src = Paths.get(path);
+
+    try {
+
+      List<String> content = Files.readAllLines(src);
+
+      List<String[]> rowsArray = new ArrayList<>();
+
+      int[] numbers = new int[90];
+
+      for (String line : content){
+
+        String[] arr = line.split(";");
+
+        rowsArray.add(arr);
+      }
+
+      for (String[] row : rowsArray){
+
+        for (int i = 0; i < 5; i++) {
+
+          String num = row[row.length - 1 - i];
+
+          int index = Integer.valueOf(num);
+          System.out.println(index);
+
+          numbers[index - 1] += 1;
+        }
+
+      }
+
+      System.out.println(Arrays.toString(numbers));
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
 
   }
 }
