d8b811b924fe2dc629e86d8f1f3c966f52b76224
Gergely Kocsis <koger23@gmail.com>
2019-04-16 11:54:31 +0200

    feat(Logs): readFile return type modified.

--- week-03/day-02/src/Logs.java
+++ week-03/day-02/src/Logs.java
@@ -3,6 +3,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Logs {
@@ -14,39 +15,49 @@ public class Logs {
     // Write a function that returns an array with the unique IP adresses.
     // Write a function that returns the GET / POST request ratio.
 
-    List<String> log = readFile("assets/log.txt");
+    List<String[]> log = readFile("assets/log.txt");
 
-    getUniqIPs(log);
+    System.out.println(Arrays.toString(log.get(0)));
 
   }
 
-  public static List<String> readFile(String path){
+  public static List<String[]> readFile(String path){
 
     Path src = Paths.get(path);
+    String sep = "   ";
 
     try {
-      List<String> content = Files.readAllLines(src);
-      return content;
+      List<String> data = Files.readAllLines(src);
+
+      List<String[]> sepContent = new ArrayList<>();
+
+      for (String s : data) {
+        s = s.replace(" /", "");
+        String[] arr = s.split(sep);
+        sepContent.add(arr);
+      }
+
+      return sepContent;
 
     } catch (IOException e) {
       System.out.println("Error while opening the file.");
 
-      List<String> content = new ArrayList<>();
+      List<String[]> emptyList = new ArrayList<>();
 
-      return content;
+      return emptyList;
     }
 
 
   }
 
-  public static List<String> getUniqIPs(List<String> fileContent){
+  public static List<String> getUniqIPs(List<String[]> fileContent){
 
     List<String> uniqIPs = new ArrayList<>();
 
     return uniqIPs;
   }
 
-  public static int getGetPostRatio(List<String> fileContent){
+  public static int getGetPostRatio(List<String[]> fileContent){
 
     int ratio = 0;
 
