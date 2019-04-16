9f741498b3777d06048f4a0492e4012d4737a69c
Gergely Kocsis <koger23@gmail.com>
2019-04-16 15:05:23 +0200

    feat(Lottery): init

---
+++ week-03/day-02/src/Lottery.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Lottery {
+
+  public static void main(String[] args) {
+    // Create a method that find the 5 most common lottery numbers in lottery.csv
+
+    getMostCommonNums();
+  }
+
+  public static void getMostCommonNums(){
+
+  }
+}
