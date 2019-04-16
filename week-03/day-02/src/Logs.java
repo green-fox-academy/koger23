3a80a850e7e81cd7e3b0c6e427a4320c1e9b81e8
Gergely Kocsis <koger23@gmail.com>
2019-04-16 11:33:51 +0200

    feat(Logs): init

---
+++ week-03/day-02/src/Logs.java
@@ -0,0 +1,30 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Logs {
+
+  public static void main(String[] args) {
+
+    // Read all data from 'log.txt'.
+    // Each line represents a log message from a web server
+    // Write a function that returns an array with the unique IP adresses.
+    // Write a function that returns the GET / POST request ratio.
+
+  }
+
+  public static List<String> readFile(String path){
+
+
+  }
+
+  public static String[] getUniqIPs(List<String> fileContent){
+
+
+  }
+
+  public static int getGetPostRatio(List<String> fileContent){
+
+  }
+
+}
