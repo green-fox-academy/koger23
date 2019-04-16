4a3190f04de175c8daba235615baf0ebf06910f7
Gergely Kocsis <koger23@gmail.com>
2019-04-16 11:45:38 +0200

    feat(Logs): readFile method added

--- week-03/day-02/src/Logs.java
+++ week-03/day-02/src/Logs.java
@@ -1,5 +1,8 @@
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Logs {
@@ -11,20 +14,43 @@ public class Logs {
     // Write a function that returns an array with the unique IP adresses.
     // Write a function that returns the GET / POST request ratio.
 
+    List<String> log = readFile("assets/log.txt");
+
+    getUniqIPs(log);
+
   }
 
   public static List<String> readFile(String path){
 
+    Path src = Paths.get(path);
+
+    try {
+      List<String> content = Files.readAllLines(src);
+      return content;
+
+    } catch (IOException e) {
+      System.out.println("Error while opening the file.");
+
+      List<String> content = new ArrayList<>();
+
+      return content;
+    }
+
 
   }
 
-  public static String[] getUniqIPs(List<String> fileContent){
+  public static List<String> getUniqIPs(List<String> fileContent){
 
+    List<String> uniqIPs = new ArrayList<>();
 
+    return uniqIPs;
   }
 
   public static int getGetPostRatio(List<String> fileContent){
 
+    int ratio = 0;
+
+    return ratio;
   }
 
 }
