a38b7c7bd130328b93a02d81778b7d11e1c321f4
Gergely Kocsis <koger23@gmail.com>
2019-04-16 14:37:50 +0200

    feat(ReversedOrder): finished.

--- week-03/day-02/src/ReversedOrder.java
+++ week-03/day-02/src/ReversedOrder.java
@@ -1,12 +1,36 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReversedOrder {
 
   public static void main(String[] args) {
     // Create a method that decrypts reversed-order.txt
 
-    reorder();
+    reorder("assets/reversed-order.txt");
   }
 
-  public static void reorder(){
+  public static void reorder(String path) {
 
+    Path src = Paths.get(path);
+
+    try {
+
+      List<String> content = Files.readAllLines(src);
+
+      List<String> reorderedContent = new ArrayList<>();
+
+      for (String line : content) {
+
+        reorderedContent.add(0, line);
+      }
+      Files.write(src, reorderedContent);
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
