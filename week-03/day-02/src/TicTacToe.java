f771b9d4fa0f36e6cdb5bf09eae0b23b2c8aa755
Gergely Kocsis <koger23@gmail.com>
2019-04-16 12:19:19 +0200

    feat(TicTacToe): init

---
+++ week-03/day-02/src/TicTacToe.java
@@ -0,0 +1,24 @@
+public class TicTacToe {
+
+  public static void main(String[] args) {
+    // Write a function that takes a filename as a parameter
+    // The file contains an ended Tic-Tac-Toe match
+    // We have provided you some example files (draw.txt, win-x.txt, win-o.txt)
+    // Return "X", "O" or "Draw" based on the input file
+
+    System.out.println(ticTacResult("assets/win-o.txt"));
+    // Should print "O"
+
+    System.out.println(ticTacResult("assets/win-x.txt"));
+    // Should print "X"
+
+    System.out.println(ticTacResult("assets/draw.txt"));
+    // Should print "Draw"
+  }
+
+  public static String ticTacResult(String path){
+
+
+    return "s";
+  }
+}
