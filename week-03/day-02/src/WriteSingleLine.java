11804c9fae1e1f8c12d0fc108233985fc5d6141c
Gergely Kocsis <koger23@gmail.com>
2019-04-16 11:05:46 +0200

    feat(WriteSingleLine): finished.

--- week-03/day-02/src/WriteSingleLine.java
+++ week-03/day-02/src/WriteSingleLine.java
@@ -1,6 +1,8 @@
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.List;
 
 public class WriteSingleLine {
   public static void main(String[] args) {
@@ -10,9 +12,24 @@ public class WriteSingleLine {
     // In case the program is unable to write the file,
     // It should print the following error message: "Unable to write file: my-file.txt"
 
+    Path src = Paths.get("assets/my-file.txt");
+
+    writeSingleLine(src);
+
   }
 
-  public static void writeSingleLine(){
-    
+  public static void writeSingleLine(Path path){
+
+    try {
+      List<String> lines = Files.readAllLines(path);
+
+      lines.add("Gergely Kocsis");
+
+      Files.write(path, lines);
+
+    } catch (IOException e) {
+      System.out.println("Unable to write file: my-file.txt");
+    }
+
   }
 }
