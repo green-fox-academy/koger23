9d895160ae1259b7a493c96ed04e40222cd6b510
Gergely Kocsis <koger23@gmail.com>
2019-04-17 14:40:57 +0200

    feat(Pirates): init

---
+++ week-03/day-03/src/Pirate.java
@@ -0,0 +1,31 @@
+public class Pirate {
+
+  private String name;
+  private boolean isIntoxicated;
+  private boolean hasParrot;
+  private boolean isAlive;
+
+  public Pirate(String name, boolean isIntoxicated, boolean hasParrot) {
+    this.name = name;
+    this.isIntoxicated = isIntoxicated;
+    this.hasParrot = hasParrot;
+    this.isAlive = true;
+  }
+
+  public void drinkSomeRum(){
+    
+  }
+
+  public void howsItGoingMate(){
+
+  }
+
+  public void die(){
+
+  }
+
+  public void brawl(Pirate pirate){
+
+  }
+
+}
