391cf5879856d44591b1fa6e77deac707b7f8dcb
Gergely Kocsis <koger23@gmail.com>
2019-04-17 16:57:12 +0200

    feat(Battle): finished.

--- week-03/day-03/src/BattleApp.java
+++ week-03/day-03/src/BattleApp.java
@@ -2,7 +2,19 @@ public class BattleApp {
 
   public static void main(String[] args) {
 
-    
+    Ship ship1 = new Ship();
+    Ship ship2 = new Ship();
+
+    ship1.setName("Black Pearl");
+    ship2.setName("Flying Dutchman");
+
+    ship1.fillShip();
+    ship2.fillShip();
+
+    ship1.battle(ship2);
+
+    ship1.shipInfo();
+    ship2.shipInfo();
 
   }
 }
