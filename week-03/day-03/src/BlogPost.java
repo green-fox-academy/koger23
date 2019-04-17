87e0c9d051e03dad0691b5108deee01156a033d5
Gergely Kocsis <koger23@gmail.com>
2019-04-17 09:05:08 +0200

    feat(BlogPost): init

---
+++ week-03/day-03/src/BlogPost.java
@@ -0,0 +1,49 @@
+public class BlogPost {
+
+  private String authorName;
+  private String title;
+  private String text;
+  private String publicationDate;
+
+  public BlogPost(){
+  }
+
+  public BlogPost(String authorName, String title, String text, String publicationDate) {
+    this.authorName = authorName;
+    this.title = title;
+    this.text = text;
+    this.publicationDate = publicationDate;
+  }
+
+  public String getAuthorName() {
+    return authorName;
+  }
+
+  public void setAuthorName(String authorName) {
+    this.authorName = authorName;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  public String getPublicationDate() {
+    return publicationDate;
+  }
+
+  public void setPublicationDate(String publicationDate) {
+    this.publicationDate = publicationDate;
+  }
+}
