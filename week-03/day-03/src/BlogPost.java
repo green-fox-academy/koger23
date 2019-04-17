49869d63377c08c1990e8e7cc911a46efdde5d8f
Gergely Kocsis <koger23@gmail.com>
2019-04-17 09:40:57 +0200

    feat(BlogPost): finished.

--- week-03/day-03/src/BlogPost.java
+++ week-03/day-03/src/BlogPost.java
@@ -46,4 +46,30 @@ public class BlogPost {
   public void setPublicationDate(String publicationDate) {
     this.publicationDate = publicationDate;
   }
+
+  public static void main(String[] args) {
+
+    BlogPost post_1 = new BlogPost();
+    post_1.setAuthorName("John Doe");
+    post_1.setPublicationDate("2000.05.04.");
+    post_1.setTitle("Lorem Ipsum");
+    post_1.setText("Lorem ipsum dolor sit amet.");
+
+    BlogPost post_2 = new BlogPost();
+    post_2.setTitle("Wait but why");
+    post_2.setAuthorName("Tim Urban");
+    post_2.setPublicationDate("2010.10.10." );
+    post_2.setText("A popular long-form, stick-figure-illustrated blog about almost everything.");
+
+    BlogPost post_3 = new BlogPost();
+    post_3.setTitle("One Engineer Is Trying to Get IBM to Reckon With Trump");
+    post_3.setAuthorName("William Turton");
+    post_3.setPublicationDate("2017.03.28.");
+    post_3.setText("Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. " +
+            "When I asked to take his picture outside one of IBM’s New York City offices, " +
+            "he told me that he wasn’t really into the whole organizer profile thing.");
+
+
+  }
+
 }
