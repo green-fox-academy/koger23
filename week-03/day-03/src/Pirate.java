e2da3cc0b63ed6d62b41ffcd21a0434aa4242e9f
Gergely Kocsis <koger23@gmail.com>
2019-04-17 14:53:24 +0200

    feat(Pirates): finished.

--- week-03/day-03/src/Pirate.java
+++ week-03/day-03/src/Pirate.java
@@ -13,19 +13,81 @@ public class Pirate {
   }
 
   public void drinkSomeRum(){
-    
+    if (isAlive){
+      this.isIntoxicated = true;
+    } else {
+      System.out.println("He is dead.");
+    }
+
   }
 
   public void howsItGoingMate(){
+    if (isAlive){
+      if (this.isIntoxicated){
+        for (int i = 0; i < 5; i++){
+          System.out.println("Pour me anudder!");
+        }
+      } else {
+        System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+      }
+    } else {
+      System.out.println("He is dead.");
+    }
 
   }
 
   public void die(){
-
+    setAlive(false);
   }
 
   public void brawl(Pirate pirate){
+    if (isAlive){
+      int result = (int) (Math.random()*3);
+
+      if (result == 0){
+        die();
+      } else if (result == 1){
+        pirate.die();
+      } else {
+        pirate.die();
+        die();
+      }
+
+    } else {
+      System.out.println("He is dead.");
+    }
 
   }
 
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public boolean isIntoxicated() {
+    return isIntoxicated;
+  }
+
+  public void setIntoxicated(boolean intoxicated) {
+    isIntoxicated = intoxicated;
+  }
+
+  public boolean isHasParrot() {
+    return hasParrot;
+  }
+
+  public void setHasParrot(boolean hasParrot) {
+    this.hasParrot = hasParrot;
+  }
+
+  public boolean isAlive() {
+    return isAlive;
+  }
+
+  public void setAlive(boolean alive) {
+    isAlive = alive;
+  }
 }
