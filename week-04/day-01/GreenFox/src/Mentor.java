d97db7cfa41c8db297be537180fade7c28c23af3
Gergely Kocsis <koger23@gmail.com>
2019-04-21 10:38:18 +0200

    feat(Mentor): finished.

--- week-04/day-01/GreenFox/src/Mentor.java
+++ week-04/day-01/GreenFox/src/Mentor.java
@@ -2,22 +2,24 @@ public class Mentor {
   public String name;
   public int age;
   public String gender;
+  public String level;
 
   public void introduce(){
-    System.out.println( "Hi, I'm " + name + ", a " + age + " year old " + gender + ".");
+    System.out.println( "Hi, I'm " + name + ", a " + age + " year old " + gender + level + "mentor.");
   }
 
   public void getGoal(){
-    System.out.println("My goal is: Live for the moment!");
+    System.out.println("Educate brilliant junior software developers.");
   }
 
   public Mentor(){
-    this("Jane Doe", 30, "female");
+    this("Jane Doe", 30, "female", "intermediate");
   }
 
-  public Mentor(String name, int age, String gender) {
+  public Mentor(String name, int age, String gender, String level) {
     this.name = name;
     this.age = age;
     this.gender = gender;
+    this.level = level;
   }
 }
