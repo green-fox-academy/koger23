3a81a53d9ea3eb246e35886f2e44347d65b5d877
Gergely Kocsis <koger23@gmail.com>
2019-04-21 10:30:01 +0200

    feat(Student): finished

--- week-04/day-01/GreenFox/src/Student.java
+++ week-04/day-01/GreenFox/src/Student.java
@@ -2,22 +2,31 @@ public class Student {
   public String name;
   public int age;
   public String gender;
+  public String previousOrganization;
+  public int skippedDays;
 
   public void introduce(){
-    System.out.println( "Hi, I'm " + name + ", a " + age + " year old " + gender + ".");
+    System.out.println( "Hi, I'm " + name + ", a " + age + " year old " + gender + "from " + previousOrganization + " who skipped " + skippedDays + ".");
   }
 
   public void getGoal(){
-    System.out.println("My goal is: Live for the moment!");
+    System.out.println("Be a junior software developer.");
+  }
+
+  public void skipDays(int numberOfDays){
+    this.skippedDays += numberOfDays;
   }
 
   public Student(){
-    this("Jane Doe", 30, "female");
+    this("Jane Doe", 30, "female", "The School of Life");
+    this.skippedDays = 0;
   }
 
-  public Student(String name, int age, String gender) {
+  public Student(String name, int age, String gender, String previousOrganization) {
     this.name = name;
     this.age = age;
     this.gender = gender;
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = 0;
   }
 }
