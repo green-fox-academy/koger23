1e1faaf3a85dcc15531247c2bc32c8778bafbc2c
Gergely Kocsis <koger23@gmail.com>
2019-04-23 14:26:10 +0200

    feat(Carrier): fill(), getMustRefillAircrafts() and getAmmoNeed() added.

--- week-04/day-01/Aircraft_Carrier/src/Carrier.java
+++ week-04/day-01/Aircraft_Carrier/src/Carrier.java
@@ -1,2 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Carrier {
+  List<Aircrafts> aircrafts = new ArrayList<>();
+  int ammoStorage;
+  int healthPoints;
+
+  public Carrier(){
+  }
+
+  public Carrier(int ammoStorage, int healthPoints) {
+    this.ammoStorage = ammoStorage;
+    this.healthPoints = healthPoints;
+  }
+
+  public void add(Aircrafts aircraft) {
+    aircrafts.add(aircraft);
+  }
+
+  public void fill() {
+    if (ammoStorage > 0) {
+      if (getAmmoNeed() <= ammoStorage){
+        for (int index : getMustRefillAircrafts()){
+          ammoStorage = aircrafts.get(index).refill(ammoStorage);
+        }
+      } else {
+        for (int index : getMustRefillAircrafts()){
+          if (aircrafts.get(index).isPriority()){
+            ammoStorage = aircrafts.get(index).refill(ammoStorage);
+          }
+        }
+      }
+    } else {
+      throw new java.lang.RuntimeException("Carrier is Out of Ammo");
+    }
+  }
+
+  public List<Integer> getMustRefillAircrafts() {
+    List<Integer> mustRefill = new ArrayList<>();
+    for (int i = 0; i < aircrafts.size(); i++) {
+      if (aircrafts.get(i).getAmmoLevel() == 0) {
+        mustRefill.add(i);
+      }
+    }
+    return mustRefill;
+  }
+
+  public int getAmmoNeed(){
+    int need = 0;
+    for (int index : getMustRefillAircrafts()){
+      need += aircrafts.get(index).getMaxAmmo();
+    }
+    return need;
+  }
 }
