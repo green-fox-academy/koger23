0a6b4dbe78a42a84ab006dd4bb1f1209e0d78bd8
Gergely Kocsis <koger23@gmail.com>
2019-04-23 10:45:09 +0200

    feat(Cohort): finished

--- week-04/day-01/GreenFox_exercise_after_inheritance/src/Cohort.java
+++ week-04/day-01/GreenFox_exercise_after_inheritance/src/Cohort.java
@@ -1,2 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cohort {
+  private String name;
+  private List<Student> students = new ArrayList<>();
+  private List<Mentor> mentors = new ArrayList<>();
+
+  public Cohort(String name){
+    this.name = name;
+    this.students.clear();
+    this.mentors.clear();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public List<Student> getStudents() {
+    return students;
+  }
+
+  public void setStudents(List<Student> students) {
+    this.students = students;
+  }
+
+  public List<Mentor> getMentors() {
+    return mentors;
+  }
+
+  public void setMentors(List<Mentor> mentors) {
+    this.mentors = mentors;
+  }
+
+  public void addStudent(Student student){
+    students.add(student);
+  }
+
+  public void addMentor(Mentor mentor){
+    mentors.add(mentor);
+  }
+
+  public void info(){
+    System.out.println("The name cohort has " + students.size() + " students and " + mentors.size() + "mentors.");
+  }
 }
