9966660a85f274a1f5aea89dbf11af8c2d3f31bc
Gergely Kocsis <koger23@gmail.com>
2019-04-23 12:36:14 +0200

    feat(GardenApp): finished.
    Output as requested.

--- week-04/day-01/The _Garden_Application/src/GardenApp.java
+++ week-04/day-01/The _Garden_Application/src/GardenApp.java
@@ -1,4 +1,20 @@
 public class GardenApp {
   public static void main(String[] args) {
+    Garden garden = new Garden();
+
+    garden.addPlant(new Flower("yellow"));
+    garden.addPlant(new Flower("blue"));
+    garden.addPlant(new Tree("purple"));
+    garden.addPlant(new Tree("orange"));
+
+    garden.printPlants();
+
+    garden.watering(40);
+    System.out.println("");
+    garden.printPlants();
+
+    garden.watering(70);
+    System.out.println("");
+    garden.printPlants();
   }
 }
