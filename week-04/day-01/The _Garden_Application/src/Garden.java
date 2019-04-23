7fc77123bbb242fee56a788cf5cfef3bceb740ff
Gergely Kocsis <koger23@gmail.com>
2019-04-23 12:06:38 +0200

    feat(Garden): plant list with methods adde

--- week-04/day-01/The _Garden_Application/src/Garden.java
+++ week-04/day-01/The _Garden_Application/src/Garden.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Garden {
+  List<Plant> plants =  new ArrayList<>();
+
+  public void addPlant(Plant plant){
+    plants.add(plant);
+  }
+  public List<Plant> getPlants() {
+    return plants;
+  }
+  public void setPlants(List<Plant> plants) {
+    this.plants = plants;
+  }
 }
