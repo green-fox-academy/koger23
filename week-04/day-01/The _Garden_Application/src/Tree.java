3be3c12b2e2685c12f3a3f1640f717c75597863d
Gergely Kocsis <koger23@gmail.com>
2019-04-23 12:34:01 +0200

    fix(Tree): color added to contructor.

--- week-04/day-01/The _Garden_Application/src/Tree.java
+++ week-04/day-01/The _Garden_Application/src/Tree.java
@@ -3,8 +3,9 @@ public class Tree extends Plant {
   private final double ABSORB_EFFICIENCY = 0.40;
   private final int WATER_ALARM_LEVEL = 10;
 
-  public Tree() {
+  public Tree(String color) {
     super();
+    super.setColor(color);
     this.setName(NAME);
     this.setAbsorbEffiecency(ABSORB_EFFICIENCY);
     this.setWater_alarm_level(WATER_ALARM_LEVEL);
