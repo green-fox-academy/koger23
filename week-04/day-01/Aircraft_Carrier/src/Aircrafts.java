99a8b4c0c00181f3ae95beef41ba9bc625404899
Gergely Kocsis <koger23@gmail.com>
2019-04-23 13:55:45 +0200

    feat(Aircraft): getters and setters

--- week-04/day-01/Aircraft_Carrier/src/Aircrafts.java
+++ week-04/day-01/Aircraft_Carrier/src/Aircrafts.java
@@ -4,19 +4,50 @@ public class Aircrafts {
   private int baseDamage;
   private int ammoLevel;
 
-  public int fight(){
+  public int fight() {
     int damage = baseDamage * ammoLevel;
     this.ammoLevel = 0;
     return damage;
   }
 
-  public int refill(int plusAmmo){
+  public int refill(int plusAmmo) {
     int newAmmo = ammoLevel + plusAmmo;
-
     if (newAmmo > maxAmmo) {
       newAmmo = maxAmmo;
     }
     ammoLevel = newAmmo;
     return plusAmmo - ammoLevel;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public int getMaxAmmo() {
+    return maxAmmo;
+  }
+
+  public void setMaxAmmo(int maxAmmo) {
+    this.maxAmmo = maxAmmo;
+  }
+
+  public int getBaseDamage() {
+    return baseDamage;
+  }
+
+  public void setBaseDamage(int baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+
+  public int getAmmoLevel() {
+    return ammoLevel;
+  }
+
+  public void setAmmoLevel(int ammoLevel) {
+    this.ammoLevel = ammoLevel;
   }
 }
