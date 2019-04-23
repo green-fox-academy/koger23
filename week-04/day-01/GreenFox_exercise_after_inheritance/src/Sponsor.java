c940f58614b89bfceba77d1777125d840a215305
Gergely Kocsis <koger23@gmail.com>
2019-04-23 10:37:56 +0200

    feat(Sponsor): finished

--- week-04/day-01/GreenFox_exercise_after_inheritance/src/Sponsor.java
+++ week-04/day-01/GreenFox_exercise_after_inheritance/src/Sponsor.java
@@ -1,2 +1,27 @@
 public class Sponsor extends Person {
+  public String company;
+  public int hiredStudents;
+
+  public void introduce(){
+    System.out.println( "Hi, I'm " + name + ", a " + age + " year old " + gender + "who represents " + company + " and hired " + hiredStudents + " students so far.");
+  }
+
+  public void getGoal(){
+    System.out.println("My goal is: Hire brilliant junior software developers.");
+  }
+
+  public void hire(){
+    this.hiredStudents++;
+  }
+
+  public Sponsor(){
+    this("Jane Doe", 30, "female", "Google");
+    this.hiredStudents = 0;
+  }
+
+  public Sponsor(String name, int age, String gender, String company) {
+    super(name, age, gender);
+    this.company = company;
+    this.hiredStudents = 0;
+  }
 }
