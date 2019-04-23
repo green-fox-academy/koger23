9c1880795e6d45da2eefb96ab695366c25596999
Gergely Kocsis <koger23@gmail.com>
2019-04-23 11:44:16 +0200

    feat(Plant): super class finished

--- week-04/day-01/The _Garden_Application/src/Plant.java
+++ week-04/day-01/The _Garden_Application/src/Plant.java
@@ -1,2 +1,50 @@
 public class Plant {
+  private String name;
+  private String color;
+  private double waterLevel;
+  private double absorbEffiecency;
+  private int water_alarm_level;
+
+  public void watering(int waterAmount){
+    this.waterLevel += waterAmount * getAbsorbEffiecency();
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getColor() {
+    return color;
+  }
+  public void setColor(String color) {
+    this.color = color;
+  }
+  public double getWaterLevel() {
+    return waterLevel;
+  }
+  public void setWaterLevel(double waterLevel) {
+    this.waterLevel = waterLevel;
+  }
+  public double getAbsorbEffiecency() {
+    return absorbEffiecency;
+  }
+  public void setAbsorbEffiecency(double absorbEffiecency) {
+    this.absorbEffiecency = absorbEffiecency;
+  }
+  public int getWater_alarm_level() {
+    return water_alarm_level;
+  }
+  public void setWater_alarm_level(int water_alarm_level) {
+    this.water_alarm_level = water_alarm_level;
+  }
+  public Plant(String name, String color, double waterLevel, double absorbEffiecency, int water_alarm_level) {
+    this.name = name;
+    this.color = color;
+    this.waterLevel = waterLevel;
+    this.absorbEffiecency = absorbEffiecency;
+    this.water_alarm_level = water_alarm_level;
+  }
+  public Plant(){
+  }
 }
