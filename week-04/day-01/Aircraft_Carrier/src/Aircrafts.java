30f0f61ddf3f3f3c1f6b6f229b48ba52308b4d87
Gergely Kocsis <koger23@gmail.com>
2019-04-23 13:44:52 +0200

    feat(Aircrafts): fight() added.

--- week-04/day-01/Aircraft_Carrier/src/Aircrafts.java
+++ week-04/day-01/Aircraft_Carrier/src/Aircrafts.java
@@ -1,2 +1,12 @@
 public class Aircrafts {
+  private String name;
+  private int maxAmmo;
+  private int baseDamage;
+  private int ammo;
+
+  public int fight(){
+    int damage = baseDamage * ammo;
+    this.ammo = 0;
+    return damage;
+  }
 }
