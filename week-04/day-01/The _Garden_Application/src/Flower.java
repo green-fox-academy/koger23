3420cf8b45ab2deae09cd86b0911fda7c0dbce39
Gergely Kocsis <koger23@gmail.com>
2019-04-23 11:33:29 +0200

    feat(Flower): finished
    methods, variables, constructor added

--- week-04/day-01/The _Garden_Application/src/Flower.java
+++ week-04/day-01/The _Garden_Application/src/Flower.java
@@ -1,2 +1,37 @@
 public class Flower {
+  private String name;
+  private String color;
+  private double waterLevel;
+  private final double ABSORB_EFFICIENCY = 0.75;
+  private final int WATER_ALARM_LEVEL = 5;
+
+  public void watering(int waterAmount){
+    this.waterLevel += waterAmount * getABSORB_EFFICIENCY();
+  }
+
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getColor() {
+    return color;
+  }
+  public void setColor(String color) {
+    this.color = color;
+  }
+  public double getWaterLevel() {
+    return waterLevel;
+  }
+  public void setWaterLevel(double waterLevel) {
+    this.waterLevel = waterLevel;
+  }
+  public double getABSORB_EFFICIENCY() {
+    return ABSORB_EFFICIENCY;
+  }
+  public int getWATER_ALARM_LEVEL() {
+    return WATER_ALARM_LEVEL;
+  }
 }
+
