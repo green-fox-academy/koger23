5e2156ad41a04d88d180830d4bcbff0c7620545e
Gergely Kocsis <koger23@gmail.com>
2019-04-23 11:55:39 +0200

    refactor(Flower): recreate class as subclass of Plant

--- week-04/day-01/The _Garden_Application/src/Flower.java
+++ week-04/day-01/The _Garden_Application/src/Flower.java
@@ -1,34 +1,13 @@
-public class Flower {
+public class Flower extends Plant{
   final private String NAME = "Flower";
-  private String color;
-  private double waterLevel;
   private final double ABSORB_EFFICIENCY = 0.75;
   private final int WATER_ALARM_LEVEL = 5;
 
-  public void watering(int waterAmount){
-    this.waterLevel += waterAmount * getABSORB_EFFICIENCY();
-  }
-
-  public String getName() {
-    return NAME;
-  }
-  public String getColor() {
-    return color;
-  }
-  public void setColor(String color) {
-    this.color = color;
-  }
-  public double getWaterLevel() {
-    return waterLevel;
-  }
-  public void setWaterLevel(double waterLevel) {
-    this.waterLevel = waterLevel;
-  }
-  public double getABSORB_EFFICIENCY() {
-    return ABSORB_EFFICIENCY;
-  }
-  public int getWATER_ALARM_LEVEL() {
-    return WATER_ALARM_LEVEL;
+  public Flower() {
+    super();
+    this.setName(NAME);
+    this.setAbsorbEffiecency(ABSORB_EFFICIENCY);
+    this.setWater_alarm_level(WATER_ALARM_LEVEL);
   }
 }
 
