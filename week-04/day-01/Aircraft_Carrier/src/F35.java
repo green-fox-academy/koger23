a2f8e3176b7e681ac3e9f3b4d20e58307b421cbe
Gergely Kocsis <koger23@gmail.com>
2019-04-23 15:25:00 +0200

    fix(F35): constructor fixed with default values.

--- week-04/day-01/Aircraft_Carrier/src/F35.java
+++ week-04/day-01/Aircraft_Carrier/src/F35.java
@@ -1,10 +1,11 @@
 public class F35 extends Aircrafts {
   String TYPE = "F35";
 
-  public void F35(){
+  public F35(){
+    super("F35", 12, 50);
   }
 
-  public void F35(int maxAmmo, int baseDamage){
+  public F35(int maxAmmo, int baseDamage){
     super.setMaxAmmo(maxAmmo);
     super.setBaseDamage(baseDamage);
     super.setType(TYPE);
