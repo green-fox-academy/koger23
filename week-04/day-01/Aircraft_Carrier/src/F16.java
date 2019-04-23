7769c39fbd0688e3788497d852543540299e8159
Gergely Kocsis <koger23@gmail.com>
2019-04-23 15:24:47 +0200

    fix(F16): constructor fixed with default values.

--- week-04/day-01/Aircraft_Carrier/src/F16.java
+++ week-04/day-01/Aircraft_Carrier/src/F16.java
@@ -1,10 +1,11 @@
 public class F16 extends Aircrafts {
   String TYPE = "F16";
 
-  public void F16(){
+  public F16(){
+    super("F16", 8, 30);
   }
 
-  public void F16(int maxAmmo, int baseDamage){
+  public F16(int maxAmmo, int baseDamage){
     super.setMaxAmmo(maxAmmo);
     super.setBaseDamage(baseDamage);
     super.setType(TYPE);
