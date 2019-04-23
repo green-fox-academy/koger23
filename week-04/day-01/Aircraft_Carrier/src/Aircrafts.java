2662a10dbe1d81bcf35ddca4a7aac7553c3957e8
Gergely Kocsis <koger23@gmail.com>
2019-04-23 13:51:54 +0200

    feat(Aircrafts): fill() method added.

--- week-04/day-01/Aircraft_Carrier/src/Aircrafts.java
+++ week-04/day-01/Aircraft_Carrier/src/Aircrafts.java
@@ -2,11 +2,21 @@ public class Aircrafts {
   private String name;
   private int maxAmmo;
   private int baseDamage;
-  private int ammo;
+  private int ammoLevel;
 
   public int fight(){
-    int damage = baseDamage * ammo;
-    this.ammo = 0;
+    int damage = baseDamage * ammoLevel;
+    this.ammoLevel = 0;
     return damage;
+  }
+
+  public int refill(int plusAmmo){
+    int newAmmo = ammoLevel + plusAmmo;
+
+    if (newAmmo > maxAmmo) {
+      newAmmo = maxAmmo;
+    }
+    ammoLevel = newAmmo;
+    return plusAmmo - ammoLevel;
   }
 }
