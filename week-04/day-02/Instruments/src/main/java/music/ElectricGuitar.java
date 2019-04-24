6a4331ddc7c37615b708fc79920f605ac7e78a14
Gergely Kocsis <koger23@gmail.com>
2019-04-24 16:05:38 +0200

    refactor(ElectricGuitar): remove final fields and play() method

--- week-04/day-02/Instruments/src/main/java/music/ElectricGuitar.java
+++ week-04/day-02/Instruments/src/main/java/music/ElectricGuitar.java
@@ -1,24 +1,17 @@
 package main.java.music;
 
 public class ElectricGuitar extends StringedInstrument {
-  private final String NAME = "Electric Guitar";
-  private final String SOUND = "Twang";
-
   public ElectricGuitar() {
-    super.setName(NAME);
-    super.setNumberOfStrings(6);
+    setName("Electric Guitar");
+    setNumberOfStrings(6);
   }
 
   public ElectricGuitar(int strings){
-    super.setName(NAME);
+    super.setName("Electric Guitar");
     super.setNumberOfStrings(strings);
   }
 
   public String sound() {
-    return SOUND;
-  }
-
-  public void play() {
-    System.out.println(NAME + ", a " + numberOfStrings + "-stringed instrument that goes " + sound());
+    return "Twang";
   }
 }
