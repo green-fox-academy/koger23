c797273c622a539303afa28402cff448337a9d1b
Gergely Kocsis <koger23@gmail.com>
2019-04-24 16:05:54 +0200

    refactor(Violin): remove final fields and play() method

--- week-04/day-02/Instruments/src/main/java/music/Violin.java
+++ week-04/day-02/Instruments/src/main/java/music/Violin.java
@@ -1,24 +1,17 @@
 package main.java.music;
 
 public class Violin extends StringedInstrument {
-  private final String NAME = "Violin";
-  private final String SOUND = "Screech";
-
   public Violin() {
-    super.setName(NAME);
+    super.setName("Violin");
     super.setNumberOfStrings(4);
   }
 
   public Violin(int strings){
-    super.setName(NAME);
+    super.setName("Violin");
     super.setNumberOfStrings(strings);
   }
 
   public String sound() {
-    return SOUND;
-  }
-
-  public void play() {
-    System.out.println(NAME + ", a " + numberOfStrings + "-stringed instrument that goes " + sound());
+    return "Screech";
   }
 }
