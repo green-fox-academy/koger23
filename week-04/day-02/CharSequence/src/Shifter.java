402bb06d90989548d28bef35ddc6b5ace1a7e181
Gergely Kocsis <koger23@gmail.com>
2019-04-24 12:28:40 +0200

    feat(Shifter): create constructor
    fields and constructor added

--- week-04/day-02/CharSequence/src/Shifter.java
+++ week-04/day-02/CharSequence/src/Shifter.java
@@ -1,4 +1,12 @@
 public class Shifter implements CharSequence {
+  String stringToBeShifted;
+  int shiftIndex;
+
+  public Shifter(String stringToBeShifted, int shiftIndex) {
+    this.stringToBeShifted = stringToBeShifted;
+    this.shiftIndex = shiftIndex;
+  }
+
   public int length() {
     return 0;
   }
