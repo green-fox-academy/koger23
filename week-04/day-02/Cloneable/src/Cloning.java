72ed787d75eddbc1ca37b71d6af8e94d5fe93ce5
Gergely Kocsis <koger23@gmail.com>
2019-04-24 11:13:41 +0200

    feat(Cloning): finish cloning

--- week-04/day-02/Cloneable/src/Cloning.java
+++ week-04/day-02/Cloneable/src/Cloning.java
@@ -1,5 +1,10 @@
 public class Cloning {
-  public static void main(String[] args) {
-    
+  public static void main(String[] args) throws CloneNotSupportedException {
+    Person student_1 = new Student("Max", 30, "male", "Google");
+    Person student_2 = (Person) ((Student) student_1).clone();
+
+    student_2.setName("Peter");
+    System.out.println(student_1.getName());
+    System.out.println(student_2.getName());
   }
 }
