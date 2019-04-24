48229cf4e3396833638cbe305d604bdeab5717c6
Gergely Kocsis <koger23@gmail.com>
2019-04-24 16:14:49 +0200

    feat(Bird): implement Flyable

--- week-04/day-02/Flyable/src/Bird.java
+++ week-04/day-02/Flyable/src/Bird.java
@@ -1,8 +1,20 @@
-public class Bird extends Animal {
+public class Bird extends Animal implements Flyable {
   public Bird(String name) {
     setName(name);
   }
   public String breed() {
     return "laying eggs";
+  }
+
+  @Override
+  public void land() {
+  }
+
+  @Override
+  public void fly() {
+  }
+
+  @Override
+  public void takeOff() {
   }
 }
