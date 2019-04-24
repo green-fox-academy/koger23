f4884048c1e76a1a8e85c8a67ec9b059cb7c0711
Gergely Kocsis <koger23@gmail.com>
2019-04-24 15:42:29 +0200

    feat(Animal): add setters and getters

--- week-04/day-02/Zoo/src/main/java/animals/Animal.java
+++ week-04/day-02/Zoo/src/main/java/animals/Animal.java
@@ -4,10 +4,22 @@ public abstract class Animal {
   private String name;
   private int age;
 
+  public void setName(String name){
+    this.name = name;
+  }
+
   public String getName(){
     return name;
   }
 
-  abstract void breed();
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  abstract String breed();
 
 }
