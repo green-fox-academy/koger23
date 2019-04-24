fe1ebacb344a48fdcf80acfd706b34f6490f68b2
Gergely Kocsis <koger23@gmail.com>
2019-04-24 16:05:27 +0200

    refactor(BassGuitar): remove final fields and play() method

--- week-04/day-02/Instruments/src/main/java/music/BassGuitar.java
+++ week-04/day-02/Instruments/src/main/java/music/BassGuitar.java
@@ -1,24 +1,17 @@
 package main.java.music;
 
 public class BassGuitar extends StringedInstrument {
-  private final String NAME = "Bass Guitar";
-  private final String SOUND = "Duum-duum-duum";
-
   public BassGuitar() {
-    super.setName(NAME);
+    super.setName("Bass Guitar");
     super.setNumberOfStrings(4);
   }
 
   public BassGuitar(int strings) {
-    super.setName(NAME);
+    super.setName("Bass Guitar");
     super.setNumberOfStrings(strings);
   }
 
   public String sound() {
-    return SOUND;
-  }
-
-  public void play() {
-    System.out.println(NAME + ", a " + numberOfStrings + "-stringed instrument that goes " + sound());
+    return "Duum-duum-duum";
   }
 }
