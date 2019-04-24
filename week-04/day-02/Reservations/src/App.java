f1f221d5727c99b2e1aa79f63f044d2bd2cdd297
Gergely Kocsis <koger23@gmail.com>
2019-04-24 17:04:38 +0200

    feat(App): print some random reservations

--- week-04/day-02/Reservations/src/App.java
+++ week-04/day-02/Reservations/src/App.java
@@ -1,3 +1,10 @@
-public class App {
+import main.java.booking.Reservation;
 
+public class App {
+  public static void main(String[] args) {
+    Reservation reservation = new Reservation();
+    for (int i = 0; i < 8; i++){
+      System.out.println("Booking# " + reservation.getCodeBooking() + " for " + reservation.getDowBooking());
+    }
+  }
 }
