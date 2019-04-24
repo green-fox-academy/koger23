271eda2f214afb062db4eb8090951e0c9fec1fbf
Gergely Kocsis <koger23@gmail.com>
2019-04-24 15:16:08 +0200

    feat(StringedInstrument): finished

--- week-04/day-02/Instruments/src/main/java/music/StringedInstrument.java
+++ week-04/day-02/Instruments/src/main/java/music/StringedInstrument.java
@@ -1,4 +1,15 @@
 package main.java.music;
 
 public abstract class StringedInstrument extends Instrument{
+  public int numberOfStrings;
+
+  public void setNumberOfStrings(int numOfStrings){
+    this.numberOfStrings = numOfStrings;
+  }
+
+  public abstract String sound();
+
+  public void play(){
+    System.out.println(getName() + ", a " + numberOfStrings + "-stringed instrument that goes ");
+  }
 }
