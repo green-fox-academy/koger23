5fb0a62eebf16337785708818c95163aa7ad807b
Gergely Kocsis <koger23@gmail.com>
2019-04-24 15:15:51 +0200

    feat(Instrument): finished

--- week-04/day-02/Instruments/src/main/java/music/Instrument.java
+++ week-04/day-02/Instruments/src/main/java/music/Instrument.java
@@ -1,4 +1,15 @@
 package main.java.music;
 
 public abstract class Instrument {
+  public String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public abstract void play();
 }
