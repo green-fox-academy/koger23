b00dd2d86cf93008074dce6b81509fe2804c605c
Gergely Kocsis <koger23@gmail.com>
2019-04-24 12:26:38 +0200

    feat(Shifter): init

---
+++ week-04/day-02/CharSequence/src/Shifter.java
@@ -0,0 +1,13 @@
+public class Shifter implements CharSequence {
+  public int length() {
+    return 0;
+  }
+
+  public char charAt(int i) {
+    return 0;
+  }
+
+  public CharSequence subSequence(int i, int i1) {
+    return null;
+  }
+}
