c1177bf81505a7634e004d15dc3474c78cc3e247
Gergely Kocsis <koger23@gmail.com>
2019-04-24 16:10:06 +0200

    fix(Mammal): remove super before setName()

--- week-04/day-02/Zoo/src/main/java/animals/Mammal.java
+++ week-04/day-02/Zoo/src/main/java/animals/Mammal.java
@@ -2,7 +2,7 @@ package main.java.animals;
 
 public class Mammal extends Animal {
   public Mammal(String name) {
-    super.setName(name);
+    setName(name);
   }
   public String breed() {
     return "pushing miniature versions out";
