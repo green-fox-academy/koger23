35fbabce73d366271ba9ea480660c78cf787989d
Gergely Kocsis <koger23@gmail.com>
2019-04-24 16:09:51 +0200

    fix(Bird): remove super before setName()

--- week-04/day-02/Zoo/src/main/java/animals/Bird.java
+++ week-04/day-02/Zoo/src/main/java/animals/Bird.java
@@ -2,7 +2,7 @@ package main.java.animals;
 
 public class Bird extends Animal {
   public Bird(String name) {
-    super.setName(name);
+    setName(name);
   }
   public String breed() {
     return "laying eggs";
