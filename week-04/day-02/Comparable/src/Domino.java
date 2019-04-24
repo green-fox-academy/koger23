2f3f07a43a61bd106236032c420e7a23e5791c65
Gergely Kocsis <koger23@gmail.com>
2019-04-24 11:30:20 +0200

    feat(Domino): implement Comaprable

--- week-04/day-02/Comparable/src/Domino.java
+++ week-04/day-02/Comparable/src/Domino.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class Domino {
+public class Domino implements Comparable<Domino> {
   private final int left;
   private final int right;
 
@@ -21,4 +21,10 @@ public class Domino {
   public String toString() {
     return "[" + left + ", " + right + "]";
   }
+
+  @Override
+  public int compareTo(Domino domino) {
+    return 0;
+  }
 }
+
