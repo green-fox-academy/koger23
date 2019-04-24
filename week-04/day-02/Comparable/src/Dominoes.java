319309609cc065649d453b9b08ea8fe5c5d48361
Gergely Kocsis <koger23@gmail.com>
2019-04-24 16:26:32 +0200

    feat(Dominoes): use new method from Printable interface in Domino

--- week-04/day-02/Comparable/src/Dominoes.java
+++ week-04/day-02/Comparable/src/Dominoes.java
@@ -12,6 +12,10 @@ public class Dominoes {
     // eg: [2, 4], [4, 3], [3, 5] ...
     System.out.println(dominoes);
 
+    for (Domino d : dominoes) {
+      d.printAllFields();
+    }
+
   }
 
   static List<Domino> initializeDominoes() {
