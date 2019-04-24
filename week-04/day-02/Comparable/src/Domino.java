b29892d5ca5aefc06b3af2fe2d29344a0bfa416e
Gergely Kocsis <koger23@gmail.com>
2019-04-24 16:25:57 +0200

    feat(Domino): implement Printable interface

--- week-04/day-02/Comparable/src/Domino.java
+++ week-04/day-02/Comparable/src/Domino.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class Domino implements Comparable<Domino> {
+public class Domino implements Comparable<Domino>, Printable {
   private final int left;
   private final int right;
 
@@ -25,6 +25,10 @@ public class Domino implements Comparable<Domino> {
   @Override
   public int compareTo(Domino o) {
     return toString().compareTo(o.toString());
+  }
+
+  public void printAllFields() {
+    System.out.println(toString());
   }
 }
 
