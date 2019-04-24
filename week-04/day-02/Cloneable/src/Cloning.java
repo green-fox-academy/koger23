a0c52d730e1ece6886cf2e6aa969ca1631643c17
Gergely Kocsis <koger23@gmail.com>
2019-04-24 11:16:06 +0200

    refactor(Cloning): change parameters according to exercise

--- week-04/day-02/Cloneable/src/Cloning.java
+++ week-04/day-02/Cloneable/src/Cloning.java
@@ -1,10 +1,10 @@
 public class Cloning {
   public static void main(String[] args) throws CloneNotSupportedException {
-    Person student_1 = new Student("Max", 30, "male", "Google");
-    Person student_2 = (Person) ((Student) student_1).clone();
+    Person student_1 = new Student("John", 20, "male", "BME");
+    Person jonhTheClone = (Person) ((Student) student_1).clone();
 
-    student_2.setName("Peter");
+    jonhTheClone.setName("John The Clone");
     System.out.println(student_1.getName());
-    System.out.println(student_2.getName());
+    System.out.println(jonhTheClone.getName());
   }
 }
