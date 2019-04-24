f722630e4b1bad172b9afed2dcddd9cd9d9d3779
Gergely Kocsis <koger23@gmail.com>
2019-04-24 11:12:05 +0200

    feat(Person): add setters and getters

--- week-04/day-02/Cloneable/src/Person.java
+++ week-04/day-02/Cloneable/src/Person.java
@@ -3,6 +3,30 @@ public class Person {
   public int age;
   public String gender;
 
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
   public void introduce(){
     String message = "Hi, I'm " + name + ", a " + age + " year old " + gender + ".";
     System.out.println(message);
