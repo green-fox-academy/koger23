8af5027db57020dd6f61e60c3c11e81f5c3df8bb
Gergely Kocsis <koger23@gmail.com>
2019-04-24 12:39:34 +0200

    feat(Gnirts): finish CharSequence methods

--- week-04/day-02/CharSequence/src/Gnirts.java
+++ week-04/day-02/CharSequence/src/Gnirts.java
@@ -1,13 +1,30 @@
 public class Gnirts implements CharSequence {
+  String word;
+  public Gnirts(String word) {
+    this.word = word;
+  }
+
   public int length() {
-    return 0;
+    return word.length();
   }
 
   public char charAt(int i) {
-    return 0;
+    int lastIndex = word.length() - 1;
+    return word.charAt(lastIndex - i);
   }
 
   public CharSequence subSequence(int i, int i1) {
-    return null;
+    int lastIndex = word.length() - 1;
+    return word.subSequence(lastIndex - i, lastIndex - i1);
+  }
+
+  public static void main(String[] args) {
+    Gnirts g = new Gnirts("example");
+    System.out.println(g.charAt(1));
+    // should print out: l
+
+    Shifter s = new Shifter("example", 2);
+    System.out.println(s.charAt(0));
+    // should print out: a
   }
 }
