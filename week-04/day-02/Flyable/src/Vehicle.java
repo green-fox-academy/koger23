dfcc2d774f85cb552daf8361ddb78dbea288bc1c
Gergely Kocsis <koger23@gmail.com>
2019-04-24 15:54:40 +0200

    feat(Vehicle): add fields, constructors, setters and getters

--- week-04/day-02/Flyable/src/Vehicle.java
+++ week-04/day-02/Flyable/src/Vehicle.java
@@ -1,2 +1,38 @@
 public abstract class Vehicle {
+  public int numberOfWheels;
+  public int numberOfDoors;
+  public int performance;
+
+  public Vehicle() {
+  }
+
+  public Vehicle(int numberOfWheels, int numberOfDoors, int performance) {
+    this.numberOfWheels = numberOfWheels;
+    this.numberOfDoors = numberOfDoors;
+    this.performance = performance;
+  }
+
+  public int getNumberOfWheels() {
+    return numberOfWheels;
+  }
+
+  public void setNumberOfWheels(int numberOfWheels) {
+    this.numberOfWheels = numberOfWheels;
+  }
+
+  public int getNumberOfDoors() {
+    return numberOfDoors;
+  }
+
+  public void setNumberOfDoors(int numberOfDoors) {
+    this.numberOfDoors = numberOfDoors;
+  }
+
+  public int getPerformance() {
+    return performance;
+  }
+
+  public void setPerformance(int performance) {
+    this.performance = performance;
+  }
 }
