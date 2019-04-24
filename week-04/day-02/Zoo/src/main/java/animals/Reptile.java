93541e88678c265a105763b9d566d2bde494f577
Gergely Kocsis <koger23@gmail.com>
2019-04-24 15:45:34 +0200

    feat(Reptile): add contructor and breed method

--- week-04/day-02/Zoo/src/main/java/animals/Reptile.java
+++ week-04/day-02/Zoo/src/main/java/animals/Reptile.java
@@ -1,4 +1,11 @@
 package main.java.animals;
 
 public class Reptile extends Animal {
+  public Reptile(String name) {
+    super.setName(name);
+  }
+
+  public String breed() {
+    return "laying eggs";
+  }
 }
