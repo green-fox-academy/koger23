bace05aa29d1aa637acbb4dd1c8d9d26cce1ddba
Gergely Kocsis <koger23@gmail.com>
2019-04-25 15:54:27 +0200

    test(CowsAndBullsTest): init

---
+++ week-04/day-03/Testing/tests/cowsandbulls/CowsAndBullsTest.java
@@ -0,0 +1,16 @@
+package cowsandbulls;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CowsAndBullsTest {
+
+  @Test
+  public void createFourDigitNumber() {
+  }
+
+  @Test
+  public void guess() {
+  }
+}
