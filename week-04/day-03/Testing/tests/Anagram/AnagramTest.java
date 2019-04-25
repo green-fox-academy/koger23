8a73d5818068857ee2e6b6a60a70ff24896c01ee
Gergely Kocsis <koger23@gmail.com>
2019-04-25 11:42:16 +0200

    refactor(AnagramTest): rename anagram() to anagramTrue()

--- week-04/day-03/Testing/tests/Anagram/AnagramTest.java
+++ week-04/day-03/Testing/tests/Anagram/AnagramTest.java
@@ -1,12 +1,27 @@
 package Anagram;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class AnagramTest {
 
+  Anagram anag;
+
+  @Before
+  public void setup(){
+     anag = new Anagram();
+  }
+
   @Test
-  public void anagram() {
+  public void anagramTrue() {
+    String inputWord_1 = "greenfox";
+    String inputWord_2 = "foxgreen";
+
+    boolean result = anag.anagram(inputWord_1, inputWord_2);
+    boolean expect = true;
+
+    assertEquals(expect, result);
   }
 }
