bf11ce43229292d53d5b511109f279f8ff3dc66e
Gergely Kocsis <koger23@gmail.com>
2019-04-25 14:49:32 +0200

    feat(Sharpie): copy class in

---
+++ week-04/day-03/Testing/src/sharpie/Sharpie.java
@@ -0,0 +1,44 @@
+package sharpie;
+
+public class Sharpie {
+
+  private String color;
+  private double width;
+  private double inkAmount;
+
+  public Sharpie(String color, double width) {
+    this.width = width;
+    this.color = color;
+    this.inkAmount = 100;
+  }
+
+  public void use() {
+    if (inkAmount != 0) {
+      this.inkAmount--;
+    }
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public double getWidth() {
+    return width;
+  }
+
+  public void setWidth(double width) {
+    this.width = width;
+  }
+
+  public double getInkAmount() {
+    return inkAmount;
+  }
+
+  public void setInkAmount(double inkAmount) {
+    this.inkAmount = inkAmount;
+  }
+}
