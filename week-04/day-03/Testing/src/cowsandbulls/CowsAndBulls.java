43cd91096341ac5c08f1af3dc92dae64fd7678fb
Gergely Kocsis <koger23@gmail.com>
2019-04-25 15:38:49 +0200

    feat(CowsAndBulls): init

---
+++ week-04/day-03/Testing/src/cowsandbulls/CowsAndBulls.java
@@ -0,0 +1,16 @@
+package cowsandbulls;
+
+public class CowsAndBulls {
+  private int[] charNumberList = new int[4];
+  private int fourDigitNumber = 0;
+  private String state = "Stored";
+  private int guessCounter = 0;
+
+  public void createFourDigitNumber() {
+  }
+
+  public String guess() {
+    return "";
+  }
+
+}
