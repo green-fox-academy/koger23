abb8550cabca49638b4244803a215f26bb6278e5
Gergely Kocsis <koger23@gmail.com>
2019-04-25 15:05:15 +0200

    fix(SharpieTest): work out all the methods

--- week-04/day-03/Testing/tests/sharpie/SharpieTest.java
+++ week-04/day-03/Testing/tests/sharpie/SharpieTest.java
@@ -6,36 +6,55 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SharpieTest {
+  Sharpie sp;
 
   @Before
   public void setUp() {
+    sp = new Sharpie("red", 0.5);
   }
 
   @Test
   public void use() {
+    sp.use();
+    assertNotEquals(100, sp.getInkAmount());
+    assertEquals(99.0, sp.getInkAmount(), 1.0);
   }
 
   @Test
   public void getColor() {
+    assertEquals("red", sp.getColor());
+    assertNotEquals("der", sp.getColor());
   }
 
   @Test
   public void setColor() {
+    sp.setColor("green");
+    assertNotEquals("red", sp.getColor());
+    assertEquals("green", sp.getColor());
   }
 
   @Test
   public void getWidth() {
+    assertEquals(0.5, sp.getWidth(), 0.0);
+    assertEquals(1.0, sp.getWidth(), 0.5);
   }
 
   @Test
   public void setWidth() {
+    sp.setWidth(0.7);
+    assertNotEquals(0.5, sp.getWidth(), 0.);
+    assertEquals(0.5, sp.getWidth(), 0.2);
   }
 
   @Test
   public void getInkAmount() {
+    assertEquals(100, sp.getInkAmount(), 0);
   }
 
   @Test
   public void setInkAmount() {
+    sp.setInkAmount(50);
+    assertEquals(50, sp.getInkAmount(), 0);
+    assertNotEquals(100, sp.getInkAmount());
   }
 }
