d3d87966aa2e1305579bfc37e1293839a430eac6
Gergely Kocsis <koger23@gmail.com>
2019-04-25 11:59:57 +0200

    test(CountLettersTest): init

---
+++ week-04/day-03/Testing/tests/countletters/CountLettersTest.java
@@ -0,0 +1,16 @@
+package countletters;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CountLettersTest {
+
+  @Test
+  public void count() {
+  }
+
+  @Test
+  public void checkDic() {
+  }
+}
