d93e143956a20a5939631557eca57c744d64292f
Gergely Kocsis <koger23@gmail.com>
2019-04-25 15:08:06 +0200

    feat(Animal): copy from earlier

---
+++ week-04/day-03/Testing/src/animal/Animal.java
@@ -0,0 +1,49 @@
+package animal;
+
+public class Animal {
+  private int hunger;
+  private int thirst;
+
+  public Animal() {
+    this(50, 50);
+  }
+
+  public Animal(int hunger, int thirst) {
+    this.hunger = hunger;
+    this.thirst = thirst;
+  }
+
+  public int getHunger() {
+    return hunger;
+  }
+
+  public void setHunger(int hunger) {
+    this.hunger = hunger;
+  }
+
+  public int getThirst() {
+    return thirst;
+  }
+
+  public void setThirst(int thirst) {
+    this.thirst = thirst;
+  }
+
+  public void eat() {
+    if (this.hunger != 0) {
+      this.hunger--;
+    }
+  }
+
+  public void drink() {
+    if (this.thirst != 0) {
+      this.thirst--;
+    }
+  }
+
+  public void play() {
+    this.thirst++;
+    this.hunger++;
+  }
+
+}
