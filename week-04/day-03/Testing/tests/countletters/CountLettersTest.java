694094f1cfe8a62eac7f6d479927c554fa42f8cb
Gergely Kocsis <koger23@gmail.com>
2019-04-25 12:25:48 +0200

    feat(count): work out Test

--- week-04/day-03/Testing/tests/countletters/CountLettersTest.java
+++ week-04/day-03/Testing/tests/countletters/CountLettersTest.java
@@ -1,13 +1,40 @@
 package countletters;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class CountLettersTest {
+  CountLetters cntl;
+
+  @Before
+  public void setup() {
+    cntl = new CountLetters();
+  }
 
   @Test
   public void count() {
+    String input = "kappa";
+    Map<String, Integer> expectDic = new HashMap<>();
+    expectDic.put("k", 1);
+    expectDic.put("a", 2);
+    expectDic.put("p", 2);
+    Map actualDic = cntl.count(input);
+
+    assertThat(actualDic, is(expectDic));
+    assertThat(actualDic.size(), is(3));
+    assertTrue(actualDic.containsKey("k"));
+    assertTrue(actualDic.containsKey("a"));
+    assertTrue(actualDic.containsKey("p"));
+    assertTrue(actualDic.containsValue(2));
+    assertTrue(actualDic.containsValue(1));
+    assertFalse(actualDic.containsValue(0));
+    assertFalse(actualDic.containsValue(3));
   }
 
   @Test
