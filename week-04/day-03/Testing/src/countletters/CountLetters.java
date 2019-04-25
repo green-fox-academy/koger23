afb66809984a85dd4706d07ad1915406453cd2f2
Gergely Kocsis <koger23@gmail.com>
2019-04-25 11:59:03 +0200

    feat(CountLetters): add count() and checkDic()

--- week-04/day-03/Testing/src/countletters/CountLetters.java
+++ week-04/day-03/Testing/src/countletters/CountLetters.java
@@ -1,4 +1,29 @@
 package countletters;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountLetters {
+  Map<String, Integer> dic = new HashMap();
+
+  public void count(String word){
+    char[] charList = word.toCharArray();
+
+    for (char c : charList){
+      if (checkDic(c)){
+        int prevVal = dic.get(c);
+        dic.put(String.valueOf(c), prevVal + 1);
+      } else {
+        dic.put(String.valueOf(c), 0);
+      }
+    }
+  }
+
+  public boolean checkDic(char c){
+    if (dic.containsKey(c)){
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
