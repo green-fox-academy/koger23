53ed02d66e2c7632d0a9454661a43d14e9166e44
Gergely Kocsis <koger23@gmail.com>
2019-04-25 15:09:11 +0200

    test(AnimalTest): init

---
+++ week-04/day-03/Testing/tests/animal/AnimalTest.java
@@ -0,0 +1,41 @@
+package animal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AnimalTest {
+
+  @Before
+  public void setUp() {
+  }
+
+  @Test
+  public void getHunger() {
+  }
+
+  @Test
+  public void setHunger() {
+  }
+
+  @Test
+  public void getThirst() {
+  }
+
+  @Test
+  public void setThirst() {
+  }
+
+  @Test
+  public void eat() {
+  }
+
+  @Test
+  public void drink() {
+  }
+
+  @Test
+  public void play() {
+  }
+}
