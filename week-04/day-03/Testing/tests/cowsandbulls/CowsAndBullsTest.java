def0c36702cd2ad507e331f1f4e541d04530306e
Gergely Kocsis <koger23@gmail.com>
2019-04-25 17:31:55 +0200

    feat(CowsAndBullsTest): add initNumberDigit4()
    removed createFourDigitNumber(), because it no longer exists

--- week-04/day-03/Testing/tests/cowsandbulls/CowsAndBullsTest.java
+++ week-04/day-03/Testing/tests/cowsandbulls/CowsAndBullsTest.java
@@ -1,16 +1,26 @@
 package cowsandbulls;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CowsAndBullsTest {
+  CowsAndBulls cab;
 
-  @Test
-  public void createFourDigitNumber() {
+  @Before
+  public void setUp() {
+    cab = new CowsAndBulls();
   }
 
   @Test
   public void guess() {
+  }
+
+  @Test
+  public void initNumberDigit4() {
+    int initNumber = cab.initNumber();
+
+    assertTrue(String.valueOf(initNumber).length() == 4);
   }
 }
