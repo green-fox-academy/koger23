43f8a9e6637215b742f22a304d1a2bf5498b3e0d
Gergely Kocsis <koger23@gmail.com>
2019-04-25 12:35:33 +0200

    refactor(FibonacciTest): rename fibonacciNums to fibonacciNum4th

--- week-04/day-03/Testing/tests/fibonacci/FibonacciTest.java
+++ week-04/day-03/Testing/tests/fibonacci/FibonacciTest.java
@@ -1,12 +1,24 @@
 package fibonacci;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FibonacciTest {
+  Fibonacci fibo;
+
+  @Before
+  public void setup(){
+    fibo = new Fibonacci();
+  }
 
   @Test
-  public void fibonacciNums() {
+  public void fibonacciNums4th() {
+    int elementN = 4;
+    int expected = 3;
+    int result = fibo.fibonacciNums(elementN);
+
+    assertEquals(expected, result);
   }
 }
