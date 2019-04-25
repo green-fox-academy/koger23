b8e78b0c4bdb3b2133529f1c9b7848c12c3a0d07
Gergely Kocsis <koger23@gmail.com>
2019-04-25 10:57:44 +0200

    test(ApplesTest): init

---
+++ week-04/day-03/Testing/tests/apples/ApplesTest.java
@@ -0,0 +1,15 @@
+package apples;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ApplesTest {
+  Apples apple;
+
+  @Before
+  apple = new Apples();
+
+  @Test
+  public void getApple() {
+  }
+}
