0067b3918140e5582584a21e96abc68ff8995b43
Gergely Kocsis <koger23@gmail.com>
2019-04-25 11:15:35 +0200

    feat(SumTest): add setup

--- week-04/day-03/Testing/tests/sum/SumTest.java
+++ week-04/day-03/Testing/tests/sum/SumTest.java
@@ -1,10 +1,18 @@
 package sum;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SumTest {
+
+  Sum sumObj;
+
+  @Before
+  public void setup(){
+    sumObj = new Sum();
+  }
 
   @Test
   public void sum() {
