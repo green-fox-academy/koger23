3d8ab93c0f3e9ee4daa32ed25c057002b62ec1c8
Gergely Kocsis <koger23@gmail.com>
2019-04-25 11:11:17 +0200

    feat(Apple): finish class

--- week-04/day-03/Testing/src/sum/Sum.java
+++ week-04/day-03/Testing/src/sum/Sum.java
@@ -1,4 +1,13 @@
 package sum;
 
+import java.util.ArrayList;
+
 public class Sum {
+  public int sum(ArrayList<Integer> numList){
+    int sum = 0;
+    for (Integer num : numList){
+      sum += num;
+    }
+    return sum;
+  }
 }
