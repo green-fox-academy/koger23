378039ddd937ec65028440807116a638f97be1e7
Gergely Kocsis <koger23@gmail.com>
2019-04-25 14:50:34 +0200

    test(SharpieTest): init

---
+++ week-04/day-03/Testing/tests/sharpie/SharpieTest.java
@@ -0,0 +1,41 @@
+package sharpie;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SharpieTest {
+
+  @Before
+  public void setUp() {
+  }
+
+  @Test
+  public void use() {
+  }
+
+  @Test
+  public void getColor() {
+  }
+
+  @Test
+  public void setColor() {
+  }
+
+  @Test
+  public void getWidth() {
+  }
+
+  @Test
+  public void setWidth() {
+  }
+
+  @Test
+  public void getInkAmount() {
+  }
+
+  @Test
+  public void setInkAmount() {
+  }
+}
