c53f99da961ec087020ea62db3f88e6cf96b1f86
Gergely Kocsis <koger23@gmail.com>
2019-05-14 16:14:25 +0200

    feat(Exercise11): finished

--- week-06/day-02/src/Exercise11.java
+++ week-06/day-02/src/Exercise11.java
@@ -1,11 +1,14 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class Exercise11 {
   /*
@@ -15,15 +18,41 @@ public class Exercise11 {
    * something like this:
    * */
   public static void main(String[] args) {
-    try {
-      Map<String, Integer> occurances = Files.lines(Paths.get("assets/wiki.txt"))
-              .flatMap(charSequence -> Pattern.compile("\\s+").splitAsStream(charSequence))
-              .collect(Collectors.toMap(k -> k, v -> 1, Integer::sum));
 
-      System.out.println(occurances);
+    try {
+      Files.readAllLines(Paths.get("assets/wiki.txt")).stream()
+              .flatMap(charSequence -> Pattern.compile(" ").splitAsStream(charSequence))
+              .flatMap(charSequence -> Pattern.compile("\\[").splitAsStream(charSequence))
+              .flatMap(charSequence -> Pattern.compile("\\[").splitAsStream(charSequence))
+              .flatMap(charSequence -> Pattern.compile("]").splitAsStream(charSequence))
+              .collect(Collectors.toMap(k -> {
+                        if (k.endsWith(".") || k.endsWith(",") || k.endsWith(".\n") || k.endsWith(",\n")) {
+                          return k.substring(0, k.length() - 2);
+                        } else {
+                          return k;
+                        }
+                      },
+                      v -> 1,
+                      (i, i1) -> Integer.sum(i, i1)))
+              .entrySet()
+              .stream()
+              .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+              .limit(100)
+              .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new))
+              .entrySet()
+              .stream()
+              .forEach(k -> System.out.println(k.getKey() + ": " + k.getValue()));
+
+//      System.out.println(map);
 
     } catch (IOException e) {
       e.printStackTrace();
+    }
+  }
+
+  public static void printMap(Map<String, Integer> map) {
+    for (String key : map.keySet()) {
+      System.out.println(key + ": " + map.get(key));
     }
   }
 }
