90e60f8279c143b257740f11a779b913119f7b42
Gergely Kocsis <koger23@gmail.com>
2019-05-14 11:21:48 +0200

    feat(Exercise4): finished

---
+++ week-06/day-02/src/Exercise4.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exercise4 {
+  public static void main(String[] args) {
+    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+    Double oddAverage;
+
+    oddAverage = numbers.stream()
+            .filter(n -> n % 2 != 0)
+            .collect(Collectors.averagingInt(n -> n));
+
+    System.out.println(oddAverage);
+  }
+}
