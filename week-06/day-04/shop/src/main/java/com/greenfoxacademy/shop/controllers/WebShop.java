6234a1ad90f483087a198932e5efb93f70c4ecbe
Gergely Kocsis <koger23@gmail.com>
2019-05-16 15:07:45 +0200

    feat: story 1 finished

--- week-06/day-04/shop/src/main/java/com/greenfoxacademy/shop/controllers/WebShop.java
+++ week-06/day-04/shop/src/main/java/com/greenfoxacademy/shop/controllers/WebShop.java
@@ -1,4 +1,12 @@
 package com.greenfoxacademy.shop.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WebShop {
+  @RequestMapping("/webshop")
+  public String webShop() {
+    return "Hello World";
+  }
 }
