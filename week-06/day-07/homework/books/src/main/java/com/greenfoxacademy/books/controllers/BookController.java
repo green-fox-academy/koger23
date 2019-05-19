1336ee9a6ac4fbd1bb3fa85144d3139aedbf2e02
Gergely Kocsis <koger23@gmail.com>
2019-05-19 21:26:49 +0200

    feat(BookController): add showBooks()

--- week-06/day-07/homework/books/src/main/java/com/greenfoxacademy/books/controllers/BookController.java
+++ week-06/day-07/homework/books/src/main/java/com/greenfoxacademy/books/controllers/BookController.java
@@ -1,4 +1,18 @@
 package com.greenfoxacademy.books.controllers;
 
+import com.greenfoxacademy.books.items.Book;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookController {
+  List<Book> books = new ArrayList<>();
+
+  @GetMapping(path = "/books")
+  public String showBooks(Model model) {
+    model.addAttribute("books", books);
+    return "index";
+  }
 }
