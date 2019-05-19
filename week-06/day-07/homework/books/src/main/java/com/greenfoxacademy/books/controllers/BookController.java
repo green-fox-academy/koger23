2597142bd51a602d48c27131cc86daaa6e31996b
Gergely Kocsis <koger23@gmail.com>
2019-05-19 21:33:40 +0200

    feat(BookController): add getBookById()

--- week-06/day-07/homework/books/src/main/java/com/greenfoxacademy/books/controllers/BookController.java
+++ week-06/day-07/homework/books/src/main/java/com/greenfoxacademy/books/controllers/BookController.java
@@ -3,6 +3,7 @@ package com.greenfoxacademy.books.controllers;
 import com.greenfoxacademy.books.items.Book;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,5 +15,24 @@ public class BookController {
   public String showBooks(Model model) {
     model.addAttribute("books", books);
     return "index";
+  }
+
+  @GetMapping(path = "/books/{id}/details")
+  public String getBookById(Model model, @PathVariable(name="id") Integer id) {
+    Book bookById = null;
+
+    for(Book book : books) {
+      if (book.getId() == id) {
+        bookById = book;
+      }
+    }
+
+    if (bookById != null) {
+      model.addAttribute("book", bookById);
+    } else {
+      model.addAttribute("error", "No book found");
+    }
+
+    return "details";
   }
 }
