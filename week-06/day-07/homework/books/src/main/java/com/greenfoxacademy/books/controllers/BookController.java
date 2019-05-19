9586a88af427b73df084527a9f45cf57c0afb27b
Gergely Kocsis <koger23@gmail.com>
2019-05-19 21:38:04 +0200

    feat(BookController): extend with methods
    queryBooks() and filterBooksByAuthor()

--- week-06/day-07/homework/books/src/main/java/com/greenfoxacademy/books/controllers/BookController.java
+++ week-06/day-07/homework/books/src/main/java/com/greenfoxacademy/books/controllers/BookController.java
@@ -4,9 +4,11 @@ import com.greenfoxacademy.books.items.Book;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BookController {
   List<Book> books = new ArrayList<>();
@@ -34,5 +36,26 @@ public class BookController {
     }
 
     return "details";
+  }
+
+  @GetMapping(path = "/books")
+  public String queryBooks(Model model, @RequestParam(name = "author", required = false) String author) {
+
+    List<Book> queriedBooks;
+
+    if (author != null) {
+      queriedBooks = filterBooksByAuthor(author);
+    } else {
+      queriedBooks = books;
+    }
+
+    model.addAttribute("books", queriedBooks);
+    return "index";
+  }
+
+  private List<Book> filterBooksByAuthor(String author) {
+    return books.stream()
+            .filter(book -> book.getAuthor().equals(author))
+            .collect(Collectors.toList());
   }
 }
