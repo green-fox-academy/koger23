7f4607344228246a4cccdbe6970df278402d6433
Gergely Kocsis <koger23@gmail.com>
2019-05-20 11:44:53 +0200

    feat(BankAccountController): add show endpoint

--- week-07/day-01/lionkingbank/src/main/java/com/greenfoxacademy/lionkingbank/lionkingbank/controllers/BankAccountController.java
+++ week-07/day-01/lionkingbank/src/main/java/com/greenfoxacademy/lionkingbank/lionkingbank/controllers/BankAccountController.java
@@ -1,4 +1,16 @@
 package com.greenfoxacademy.lionkingbank.lionkingbank.controllers;
 
+import com.greenfoxacademy.lionkingbank.lionkingbank.models.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BankAccountController {
+  @GetMapping("/show")
+  public String show(Model model) {
+    BankAccount bankAccount = new BankAccount("Simba", 2000, "lion");
+    model.addAttribute(bankAccount);
+    return "account";
+  }
 }
