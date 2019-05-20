fa5fd582827cca4256b9368aa0e9a774b955bba9
Gergely Kocsis <koger23@gmail.com>
2019-05-20 11:36:39 +0200

    feat(): init project

---
+++ week-07/day-01/lionkingbank/src/main/java/com/greenfoxacademy/lionkingbank/lionkingbank/LionkingbankApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.lionkingbank.lionkingbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionkingbankApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LionkingbankApplication.class, args);
+  }
+
+}
