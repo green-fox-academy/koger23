ee942583e8e75c9099f5f18fcc4907500e4c1a98
Gergely Kocsis <koger23@gmail.com>
2019-05-23 10:00:48 +0200

    feat(Food,  Drink): add name field and getters-setters

--- week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/models/Drink.java
+++ week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/models/Drink.java
@@ -1,4 +1,20 @@
 package com.greenfoxacademy.programmersfoxclub.models;
 
 public class Drink {
+  private String name;
+
+  public Drink() {
+  }
+
+  public Drink(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
--- week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/models/Food.java
+++ week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/models/Food.java
@@ -1,4 +1,20 @@
 package com.greenfoxacademy.programmersfoxclub.models;
 
 public class Food {
+  private String name;
+
+  public Food() {
+  }
+
+  public Food(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
