0ee7f3110118c1f17b1000c162d6b41a2270b974
Gergely Kocsis <koger23@gmail.com>
2019-05-23 15:49:49 +0200

    feat(Logger): add logger to handle Action History

---
+++ week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/repositories/Logger.java
@@ -0,0 +1,37 @@
+package com.greenfoxacademy.programmersfoxclub.repositories;
+
+
+import com.greenfoxacademy.programmersfoxclub.models.Nutrition;
+import org.springframework.stereotype.Repository;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public class Logger {
+  List<String> actionList = new ArrayList<>();
+
+  public Logger() {
+  }
+
+  public String getDatetime() {
+    DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+    Date date = new Date();
+    return dateFormat.format(date);
+  }
+
+  public void saveNutritionChange(Nutrition nutrition, String before, String after) {
+    String message = getDatetime() + ": " + nutrition.getType() + " changed from " + before + " to " + after;
+    actionList.add(message);
+    System.out.println(actionList.toString());
+  }
+
+  public void saveTrickAction(String trick) {
+    String message = getDatetime() + ": " + "Learned to " + trick;
+    actionList.add(message);
+    System.out.println(actionList.toString());
+  }
+}
