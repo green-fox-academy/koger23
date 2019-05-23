fc6338942185d59c34dc6ac37ca1904d83888504
Gergely Kocsis <koger23@gmail.com>
2019-05-23 10:33:08 +0200

    feat(MainController): add postMapping for /login

--- week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/controllers/MainController.java
+++ week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/controllers/MainController.java
@@ -1,7 +1,11 @@
 package com.greenfoxacademy.programmersfoxclub.controllers;
 
+import com.greenfoxacademy.programmersfoxclub.models.Fox;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -11,7 +15,14 @@ public class MainController {
   }
 
   @GetMapping("/login")
-  public String login() {
+  public String login(Model model) {
+    model.addAttribute("fox", new Fox());
     return "login";
+  }
+
+  @PostMapping("/login")
+  public String loggedIn(Model model, @ModelAttribute Fox fox) {
+    model.addAttribute("fox", fox);
+    return "redirect:/";
   }
 }
