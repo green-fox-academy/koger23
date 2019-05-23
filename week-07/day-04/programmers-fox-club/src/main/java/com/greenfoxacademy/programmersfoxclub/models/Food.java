5c19b32f78394b2b99e6775d37dae6aed092e898
Gergely Kocsis <koger23@gmail.com>
2019-05-23 15:49:15 +0200

    feat(Food): add type with extending Nutrition

--- week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/models/Food.java
+++ week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/models/Food.java
@@ -1,12 +1,14 @@
 package com.greenfoxacademy.programmersfoxclub.models;
 
-public class Food {
+public class Food extends Nutrition {
   private String name;
 
   public Food() {
+    super("Food");
   }
 
   public Food(String name) {
+    super("Food");
     this.name = name;
   }
 
