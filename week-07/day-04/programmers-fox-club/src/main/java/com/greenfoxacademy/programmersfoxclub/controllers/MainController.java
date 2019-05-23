351f918e96c6521551d69c46bcbf481cb4a13aa2
Gergely Kocsis <koger23@gmail.com>
2019-05-23 11:08:19 +0200

    feat(MainController): add postMapping for "/" and default fox

--- week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/controllers/MainController.java
+++ week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/controllers/MainController.java
@@ -1,16 +1,36 @@
 package com.greenfoxacademy.programmersfoxclub.controllers;
 
+import com.greenfoxacademy.programmersfoxclub.models.Drink;
+import com.greenfoxacademy.programmersfoxclub.models.Food;
 import com.greenfoxacademy.programmersfoxclub.models.Fox;
+import com.greenfoxacademy.programmersfoxclub.services.FoxKennel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Arrays;
+
 @Controller
 public class MainController {
+  FoxKennel foxKennel;
+
+  public MainController(FoxKennel foxKennel) {
+    this.foxKennel = foxKennel;
+  }
+
   @GetMapping("/")
-  public String home() {
+  public String home(Model model) {
+    Fox defaultFox = new Fox("Karak", new Food("pudding"), new Drink("beer"), Arrays.asList("pushups", "killing birds"));
+    foxKennel.addFox(defaultFox);
+    model.addAttribute("fox", defaultFox);
+    return "index";
+  }
+
+  @PostMapping("/")
+  public String home(Model model, @ModelAttribute Fox fox) {
+    model.addAttribute("fox", fox);
     return "index";
   }
 
