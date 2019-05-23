2aa79eb997ca622978c2f3a29ad4c124d5fcb33d
Gergely Kocsis <koger23@gmail.com>
2019-05-23 15:48:30 +0200

    feat(Nutrition): add superclass for fodd and drink type

---
+++ week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/models/Nutrition.java
@@ -0,0 +1,17 @@
+package com.greenfoxacademy.programmersfoxclub.models;
+
+public class Nutrition {
+  String type;
+
+  public Nutrition(String type) {
+    this.type = type;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+}
