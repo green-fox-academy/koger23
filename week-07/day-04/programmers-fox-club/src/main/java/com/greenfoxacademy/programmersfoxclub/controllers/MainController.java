584b4188f2c4eca1ceb243ada37f76eb40214120
Gergely Kocsis <koger23@gmail.com>
2019-05-23 16:46:49 +0200

    feat(MainController): add action history

--- week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/controllers/MainController.java
+++ week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.programmersfoxclub.controllers;
 
 import com.greenfoxacademy.programmersfoxclub.models.Fox;
+import com.greenfoxacademy.programmersfoxclub.repositories.Logger;
 import com.greenfoxacademy.programmersfoxclub.services.FoxKennel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,13 +10,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 public class MainController {
   private FoxKennel foxKennel;
+  private Logger logger;
   private int activeFoxIndex = 0;
 
-  public MainController(FoxKennel foxKennel) {
+  public MainController(FoxKennel foxKennel, Logger logger) {
     this.foxKennel = foxKennel;
+    this.logger = logger;
   }
 
   @GetMapping("/")
@@ -57,8 +61,16 @@ public class MainController {
   @GetMapping("/nutritionsave")
   public String saveNutritions(Model model, @RequestParam("food") String food,
                                @RequestParam("drink") String drink) {
+
+    String foodBefore = foxKennel.getFoxList().get(activeFoxIndex).getFood().getName();
+    String drinkBefore = foxKennel.getFoxList().get(activeFoxIndex).getDrink().getName();
+
     foxKennel.getFoxList().get(activeFoxIndex).getFood().setName(food.toLowerCase());
+    logger.saveNutritionChange(foxKennel.getFoxList().get(activeFoxIndex).getFood(), foodBefore, foxKennel.getFoxList().get(activeFoxIndex).getFood().getName());
+
     foxKennel.getFoxList().get(activeFoxIndex).getDrink().setName(drink.toLowerCase());
+    logger.saveNutritionChange(foxKennel.getFoxList().get(activeFoxIndex).getDrink(), drinkBefore, foxKennel.getFoxList().get(activeFoxIndex).getDrink().getName());
+
     model.addAttribute("fox", foxKennel.getFoxList().get(activeFoxIndex));
     return "redirect:/";
   }
@@ -70,9 +82,15 @@ public class MainController {
 
   @GetMapping("/tricksave")
   public String saveTrick(Model model, @RequestParam("trick") String trick) {
-    System.out.println(trick);
     foxKennel.getFoxList().get(activeFoxIndex).addTrick(trick);
+    logger.saveTrickAction(trick);
     model.addAttribute("fox", foxKennel.getFoxList().get(activeFoxIndex));
     return "redirect:/";
+  }
+
+  @GetMapping("/actionhistory")
+  public String actionHistory(Model model) {
+    model.addAttribute("actionHistoryLogger", logger);
+    return "actionhistory";
   }
 }
