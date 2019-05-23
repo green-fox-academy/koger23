e658581254cade56297f540eb56fdce9f5732dc1
Gergely Kocsis <koger23@gmail.com>
2019-05-23 10:41:28 +0200

    feat(FoxKennel): add methods
    add fox, get fox, getters-setters for list of fox

--- week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/services/FoxKennel.java
+++ week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/services/FoxKennel.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FoxKennel {
@@ -12,5 +13,35 @@ public class FoxKennel {
 
   public FoxKennel() {
     foxList = new ArrayList<>();
+  }
+
+  public boolean addFox(Fox fox) {
+    if (!checkFoxExists(fox.getName())) {
+      foxList.add(fox);
+      return true;
+    }
+    return false;
+  }
+
+  public Fox getFoxByName(String name) {
+    return foxList.stream()
+            .filter(fox -> fox.getName().equals(name))
+            .collect(Collectors.toList()).get(0);
+  }
+
+  public boolean checkFoxExists(String name) {
+    long result = foxList.stream().filter(fox -> fox.getName().equals(name)).count();
+    if (result > 0) {
+      return true;
+    }
+    return false;
+  }
+
+  public List<Fox> getFoxList() {
+    return foxList;
+  }
+
+  public void setFoxList(List<Fox> foxList) {
+    this.foxList = foxList;
   }
 }
