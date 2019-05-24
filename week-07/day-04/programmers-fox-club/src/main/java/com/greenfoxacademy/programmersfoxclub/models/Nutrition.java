ce142015a85c486183d5c09309cd47188007a3d1
Gergely Kocsis <koger23@gmail.com>
2019-05-24 17:17:46 +0200

    feat(Nutrition): add new fields
    MAX_LEVEL, level, reductionTimeMinutes

--- week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/models/Nutrition.java
+++ week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/models/Nutrition.java
@@ -1,7 +1,10 @@
 package com.greenfoxacademy.programmersfoxclub.models;
 
 public class Nutrition {
-  String type;
+  private String type;
+  private final int MAX_LEVEL = 10;
+  private int level;
+  private int reductionTimeMinutes;
 
   public Nutrition(String type) {
     this.type = type;
@@ -13,5 +16,25 @@ public class Nutrition {
 
   public void setType(String type) {
     this.type = type;
+  }
+
+  public int getMAX_LEVEL() {
+    return MAX_LEVEL;
+  }
+
+  public int getLevel() {
+    return level;
+  }
+
+  public void setLevel(int level) {
+    this.level = level;
+  }
+
+  public int getReductionTimeMinutes() {
+    return reductionTimeMinutes;
+  }
+
+  public void setReductionTimeMinutes(int reductionTimeMinutes) {
+    this.reductionTimeMinutes = reductionTimeMinutes;
   }
 }
