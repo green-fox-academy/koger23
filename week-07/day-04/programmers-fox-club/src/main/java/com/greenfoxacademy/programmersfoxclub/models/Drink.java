953ebc96c1ca673a1f6ffe123cb27aeff729937c
Gergely Kocsis <koger23@gmail.com>
2019-05-24 17:22:00 +0200

    feat(Drink): add reductionTime

--- week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/models/Drink.java
+++ week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/models/Drink.java
@@ -2,14 +2,17 @@ package com.greenfoxacademy.programmersfoxclub.models;
 
 public class Drink extends Nutrition {
   private String name;
+  private int reductionTimeMinutes = 1;
 
   public Drink() {
     super("Drink");
+    setReductionTimeMinutes(reductionTimeMinutes);
   }
 
   public Drink(String name) {
     super("Drink");
     this.name = name;
+    setReductionTimeMinutes(reductionTimeMinutes);
   }
 
   public String getName() {
