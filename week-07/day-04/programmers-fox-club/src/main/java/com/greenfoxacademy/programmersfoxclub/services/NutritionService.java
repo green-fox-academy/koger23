ce5b4c115aa17cac0e23e77dc7e6ce8288bbaec4
Gergely Kocsis <koger23@gmail.com>
2019-05-26 13:47:32 +0200

    fix(NutritionService): works reduceNutritionLevel() method

--- week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/services/NutritionService.java
+++ week-07/day-04/programmers-fox-club/src/main/java/com/greenfoxacademy/programmersfoxclub/services/NutritionService.java
@@ -10,30 +10,62 @@ import java.util.List;
 @Service
 public class NutritionService {
   private long startMinutes;
+  private long lastFoodMinutes = 0;
+  private long lastDrinkMinutes = 0;
 
   public NutritionService() {
-    startMinutes = DateUtils.getCurrentDateTimeInMinutes();
+    this.startMinutes = DateUtils.getCurrentDateTimeInMinutes();
+    this.lastFoodMinutes = startMinutes;
+    this.lastDrinkMinutes = startMinutes;
   }
 
   public void reduceNutritionLevel(Fox fox) {
     List<Nutrition> foxNutritionList = fox.getNutritions();
+    long currentMinutes = DateUtils.getCurrentDateTimeInMinutes();
+    long foodDelta = currentMinutes - lastFoodMinutes;
+    long drinkDelta = currentMinutes - lastDrinkMinutes;
+    boolean foodUpdateAble = false;
+    boolean drinkUpdateAble = false;
 
     for (Nutrition nutrition : foxNutritionList) {
-      long currentMinutes = DateUtils.getCurrentDateTimeInMinutes();
-
-      if (startMinutes + nutrition.getReductionTimeMinutes() <= currentMinutes) {
-        int beforeLevel = nutrition.getLevel();
-
-        if (beforeLevel > 0) {
-          nutrition.setLevel(beforeLevel - 1);
-        } else if (beforeLevel == 0) {
-          if (fox.getHealthPoints() > 0) {
-            fox.setHealthPoints(fox.getHealthPoints() - 1);
+      switch (nutrition.getType()) {
+        case FOOD:
+          double elapsedFood = foodDelta / nutrition.getReductionTimeMinutes();
+          if (elapsedFood >= 1) {
+            foodUpdateAble = true;
           }
-        }
-        if (fox.getHealthPoints() == 0) {
-          fox.setAlive(false);
-        }
+          if (lastFoodMinutes != 0 && foodUpdateAble) {
+            for (int i=0; i < elapsedFood; i++) {
+              checkLevels(fox, nutrition);
+            }
+            lastFoodMinutes = currentMinutes;
+            foodUpdateAble = false;
+          }
+        case DRINK:
+          double elapsedDrink = drinkDelta / nutrition.getReductionTimeMinutes();
+          if (elapsedDrink >= 1) {
+            drinkUpdateAble = true;
+          }
+          if (lastDrinkMinutes != 0 && drinkUpdateAble) {
+            for (int i=0; i < elapsedDrink; i++) {
+              checkLevels(fox, nutrition);
+            }
+            lastDrinkMinutes = currentMinutes;
+            drinkUpdateAble = false;
+          }
+      }
+    }
+  }
+
+  private void checkLevels(Fox fox, Nutrition nutrition) {
+    int beforeLevel = nutrition.getLevel();
+    if (beforeLevel > 0) {
+      nutrition.setLevel(beforeLevel - 1);
+    } else if (beforeLevel == 0) {
+      if (fox.getHealthPoints() > 0) {
+        fox.setHealthPoints(fox.getHealthPoints() - 1);
+      } else {
+        fox.setAlive(false);
       }
     }
   }
@@ -44,5 +76,6 @@ public class NutritionService {
     } else {
       fox.setThirstLevel(nutrition.getThirstReductionPoints());
     }
+    startMinutes = DateUtils.getCurrentDateTimeInMinutes();
   }
 }
