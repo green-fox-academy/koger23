747bea9e8f0e072b93bdfce6cebaa06c58235d9e
Gergely Kocsis <koger23@gmail.com>
2019-05-27 11:46:07 +0200

    refactor(TennisGame2): simplify getScore() method
    Using earlier created getScore() from Score class

--- week-08/day-01/tennis-game/src/TennisGame2.java
+++ week-08/day-01/tennis-game/src/TennisGame2.java
@@ -1,3 +1,4 @@
+import score.Score;
 
 public class TennisGame2 implements TennisGame {
   public int playerOneScore = 0;
@@ -14,75 +15,7 @@ public class TennisGame2 implements TennisGame {
   }
 
   public String getScore() {
-    String score = "";
-    if (playerOneScore == playerTwoScore && playerOneScore < 4) {
-      if (playerOneScore == 0)
-        score = "Love";
-      if (playerOneScore == 1)
-        score = "Fifteen";
-      if (playerOneScore == 2)
-        score = "Thirty";
-      if (playerOneScore == 3)
-        score = "Forty";
-      score += "-All";
-    }
-    if (playerOneScore == playerTwoScore && playerOneScore > 3)
-      score = "Deuce";
-    if (playerOneScore > 0 && playerTwoScore == 0) {
-      if (playerOneScore == 1)
-        P1res = "Fifteen";
-      if (playerOneScore == 2)
-        P1res = "Thirty";
-      if (playerOneScore == 3)
-        P1res = "Forty";
-      P2res = "Love";
-      score = P1res + "-" + P2res;
-    }
-    if (playerTwoScore > 0 && playerOneScore == 0) {
-      if (playerTwoScore == 1)
-        P2res = "Fifteen";
-      if (playerTwoScore == 2)
-        P2res = "Thirty";
-      if (playerTwoScore == 3)
-        P2res = "Forty";
-      P1res = "Love";
-      score = P1res + "-" + P2res;
-    }
-    if (playerOneScore > playerTwoScore && playerOneScore < 4) {
-      if (playerOneScore == 2)
-        P1res = "Thirty";
-      if (playerOneScore == 3)
-        P1res = "Forty";
-      if (playerTwoScore == 1)
-        P2res = "Fifteen";
-      if (playerTwoScore == 2)
-        P2res = "Thirty";
-      score = P1res + "-" + P2res;
-    }
-    if (playerTwoScore > playerOneScore && playerTwoScore < 4) {
-      if (playerTwoScore == 2)
-        P2res = "Thirty";
-      if (playerTwoScore == 3)
-        P2res = "Forty";
-      if (playerOneScore == 1)
-        P1res = "Fifteen";
-      if (playerOneScore == 2)
-        P1res = "Thirty";
-      score = P1res + "-" + P2res;
-    }
-    if (playerOneScore > playerTwoScore && playerTwoScore >= 3) {
-      score = "Advantage player1";
-    }
-    if (playerTwoScore > playerOneScore && playerOneScore >= 3) {
-      score = "Advantage player2";
-    }
-    if (playerOneScore >= 4 && playerTwoScore >= 0 && (playerOneScore - playerTwoScore) >= 2) {
-      score = "Win for player1";
-    }
-    if (playerTwoScore >= 4 && playerOneScore >= 0 && (playerTwoScore - playerOneScore) >= 2) {
-      score = "Win for player2";
-    }
-    return score;
+    return Score.getScore(playerOneScore, playerTwoScore);
   }
 
   public void SetP1Score(int number) {
