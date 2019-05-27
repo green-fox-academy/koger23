4efb5ed1a61a96ec361cf937af6df47a5595be29
Gergely Kocsis <koger23@gmail.com>
2019-05-27 11:32:05 +0200

    feat(TennisGame2): create setters and getters

--- week-08/day-01/tennis-game/src/TennisGame2.java
+++ week-08/day-01/tennis-game/src/TennisGame2.java
@@ -111,4 +111,52 @@ public class TennisGame2 implements TennisGame {
     else
       P2Score();
   }
+
+  public int getP1point() {
+    return P1point;
+  }
+
+  public void setP1point(int p1point) {
+    P1point = p1point;
+  }
+
+  public int getP2point() {
+    return P2point;
+  }
+
+  public void setP2point(int p2point) {
+    P2point = p2point;
+  }
+
+  public String getP1res() {
+    return P1res;
+  }
+
+  public void setP1res(String p1res) {
+    P1res = p1res;
+  }
+
+  public String getP2res() {
+    return P2res;
+  }
+
+  public void setP2res(String p2res) {
+    P2res = p2res;
+  }
+
+  public String getPlayer1Name() {
+    return player1Name;
+  }
+
+  public void setPlayer1Name(String player1Name) {
+    this.player1Name = player1Name;
+  }
+
+  public String getPlayer2Name() {
+    return player2Name;
+  }
+
+  public void setPlayer2Name(String player2Name) {
+    this.player2Name = player2Name;
+  }
 }
