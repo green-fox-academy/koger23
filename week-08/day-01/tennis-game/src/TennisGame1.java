f332f8728b2547632b434105b246d0e61653e38e
Gergely Kocsis <koger23@gmail.com>
2019-05-27 10:19:27 +0200

    refactor(TennisGame1): refactor else statement body
    new methods: getRunningScore(), getTempScore(), buildScore()

--- week-08/day-01/tennis-game/src/TennisGame1.java
+++ week-08/day-01/tennis-game/src/TennisGame1.java
@@ -20,36 +20,13 @@ public class TennisGame1 implements TennisGame {
   }
 
   public String getScore() {
-    String score = "";
-    int tempScore = 0;
     if (m_score1 == m_score2) {
       return getScoreIfDraw();
     } else if (m_score1 >= 4 || m_score2 >= 4) {
       return getScoreIfMinusResultIsOne();
     } else {
-      for (int i = 1; i < 3; i++) {
-        if (i == 1) tempScore = m_score1;
-        else {
-          score += "-";
-          tempScore = m_score2;
-        }
-        switch (tempScore) {
-          case 0:
-            score += "Love";
-            break;
-          case 1:
-            score += "Fifteen";
-            break;
-          case 2:
-            score += "Thirty";
-            break;
-          case 3:
-            score += "Forty";
-            break;
-        }
-      }
+      return getRunningScore();
     }
-    return score;
   }
 
   private String getScoreIfDraw() {
@@ -78,6 +55,44 @@ public class TennisGame1 implements TennisGame {
     } else {
       return  "Win for player2";
     }
+  }
+
+  private String getRunningScore() {
+    StringBuilder score = new StringBuilder();
+    for (int i = 1; i < 3; i++) {
+      score.append(buildScore(i));
+    }
+    return score.toString();
+  }
+
+  private int getTempScore(int i) {
+    if (i == 1) {
+      return m_score1;
+    } else {
+      return m_score2;
+    }
+  }
+
+  private String buildScore(int i) {
+    StringBuilder score = new StringBuilder();
+    if (i != 1) {
+      score.append("-");
+    }
+    switch (getTempScore(i)) {
+      case 0:
+        score.append("Love");
+        break;
+      case 1:
+        score.append("Fifteen");
+        break;
+      case 2:
+        score.append("Thirty");
+        break;
+      case 3:
+        score.append("Forty");
+        break;
+    }
+    return score.toString();
   }
 }
 
