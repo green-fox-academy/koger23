fb172907992c68d25016d1af13c7f907cc93a0fd
Gergely Kocsis <koger23@gmail.com>
2019-05-27 09:44:21 +0200

    refactor(TennisGame1): taken out body of if under new method
    The new method: getScoreIfDraw()

--- week-08/day-01/tennis-game/src/TennisGame1.java
+++ week-08/day-01/tennis-game/src/TennisGame1.java
@@ -23,24 +23,7 @@ public class TennisGame1 implements TennisGame {
     String score = "";
     int tempScore = 0;
     if (m_score1 == m_score2) {
-      switch (m_score1) {
-        case 0:
-          score = "Love-All";
-          break;
-        case 1:
-          score = "Fifteen-All";
-          break;
-        case 2:
-          score = "Thirty-All";
-          break;
-        case 3:
-          score = "Forty-All";
-          break;
-        default:
-          score = "Deuce";
-          break;
-
-      }
+      return getScoreIfDraw();
     } else if (m_score1 >= 4 || m_score2 >= 4) {
       int minusResult = m_score1 - m_score2;
       if (minusResult == 1) score = "Advantage player1";
@@ -71,6 +54,21 @@ public class TennisGame1 implements TennisGame {
       }
     }
     return score;
+  }
+
+  private String getScoreIfDraw() {
+    switch (m_score1) {
+      case 0:
+        return "Love-All";
+      case 1:
+        return "Fifteen-All";
+      case 2:
+        return "Thirty-All";
+      case 3:
+        return "Forty-All";
+      default:
+        return "Deuce";
+    }
   }
 }
 
