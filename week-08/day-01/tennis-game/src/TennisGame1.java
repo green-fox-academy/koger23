9a0b8c80641efc6f6ed3848aea945c07ee1ebaae
Gergely Kocsis <koger23@gmail.com>
2019-05-27 09:34:46 +0200

    fix(TennisGame1): fix condition in wonPoint() and style if-else
    == was used insted of .equals() for comparing strings

--- week-08/day-01/tennis-game/src/TennisGame1.java
+++ week-08/day-01/tennis-game/src/TennisGame1.java
@@ -12,10 +12,11 @@ public class TennisGame1 implements TennisGame {
   }
 
   public void wonPoint(String playerName) {
-    if (playerName == "player1")
+    if (playerName.equals("player1")) {
       m_score1 += 1;
-    else
+    } else {
       m_score2 += 1;
+    }
   }
 
   public String getScore() {
