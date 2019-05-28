15a1b5404c7021230af20d533fb1e2ac563d8177
Gergely Kocsis <koger23@gmail.com>
2019-05-28 11:13:58 +0200

    feat: init project

---
+++ week-08/day-02/listing_todo_mysql/src/main/java/com/greenfoxacademy/listing_todo_mysql/ListingTodoMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.listing_todo_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListingTodoMysqlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ListingTodoMysqlApplication.class, args);
+  }
+
+}
