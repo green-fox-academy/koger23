089201646850d2e5757cd3e47ac86fe3d8e9f24e
Gergely Kocsis <koger23@gmail.com>
2019-05-28 13:08:58 +0200

    fix(TodoService): set fields to private

--- week-08/day-02/listing_todo_mysql/src/main/java/com/greenfoxacademy/listing_todo_mysql/service/TodoService.java
+++ week-08/day-02/listing_todo_mysql/src/main/java/com/greenfoxacademy/listing_todo_mysql/service/TodoService.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 @Service
 public class TodoService {
 
-  TodoRepository todoRepository;
+  private TodoRepository todoRepository;
 
   public TodoService(TodoRepository todoRepository) {
     this.todoRepository = todoRepository;
