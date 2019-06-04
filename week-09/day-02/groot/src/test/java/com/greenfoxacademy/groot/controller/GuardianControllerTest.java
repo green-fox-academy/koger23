9bcc485a62bc36ce725ff0cbc3f45789bf8f21c2
Gergely Kocsis <koger23@gmail.com>
2019-06-04 11:47:08 +0200

    fix(GuardianController): remove lines for posting

--- week-09/day-02/groot/src/test/java/com/greenfoxacademy/groot/controller/GuardianControllerTest.java
+++ week-09/day-02/groot/src/test/java/com/greenfoxacademy/groot/controller/GuardianControllerTest.java
@@ -4,11 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.nio.charset.Charset;
 
 import static org.hamcrest.core.Is.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -19,29 +17,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(GuardianController.class)
 public class GuardianControllerTest {
 
-  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-          MediaType.APPLICATION_JSON.getSubtype(),
-          Charset.forName("utf8"));
-
   @Autowired
   private MockMvc mockMvc;
 
   @Test
   public void groot_shoudReturnOK() throws Exception {
     String message = "Test message";
-    mockMvc.perform(get("/groot/?message=" + message)
-            .contentType(MediaType.APPLICATION_JSON))
+    mockMvc.perform(get("/groot/?message=" + message))
             .andExpect(status().isOk())
-            .andExpect(content().contentType(contentType))
             .andExpect(jsonPath("$.received", is(message)));
 
   }
+  
   @Test
   public void groot_shoudReturnNotOK() throws Exception {
-    mockMvc.perform(get("/groot")
-            .contentType(MediaType.APPLICATION_JSON))
+    mockMvc.perform(get("/groot"))
             .andExpect(status().isBadRequest())
-            .andExpect(content().contentType(contentType))
             .andExpect(jsonPath("$.error", is("I am a Groot!")));
 
   }
