bcbb039e0723feb0fbb868ceac93171b16547815
Gergely Kocsis <koger23@gmail.com>
2019-06-04 11:43:33 +0200

    feat(GuardianController): create /yondu endpoint

--- week-09/day-02/groot/src/main/java/com/greenfoxacademy/groot/controller/GuardianController.java
+++ week-09/day-02/groot/src/main/java/com/greenfoxacademy/groot/controller/GuardianController.java
@@ -2,8 +2,10 @@ package com.greenfoxacademy.groot.controller;
 
 import com.greenfoxacademy.groot.DTO.MessageDTO;
 import com.greenfoxacademy.groot.DTO.NoMessageError;
+import com.greenfoxacademy.groot.DTO.YondusArrowDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +20,19 @@ public class GuardianController {
     } else {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new NoMessageError());
     }
+  }
+
+  @GetMapping("/yondu")
+  public ResponseEntity<?> yondu(Long distance, Long time) {
+    if (distance != null && time != null) {
+      return ResponseEntity.status(HttpStatus.OK).body(new YondusArrowDTO(distance, time));
+    } else {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: missing parameter time or distance.");
+    }
+  }
+
+  @ExceptionHandler(ArithmeticException.class)
+  public ResponseEntity<?> dividingByZero() {
+    return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Error: dividing by zero. Time cannot be zero!");
   }
 }
