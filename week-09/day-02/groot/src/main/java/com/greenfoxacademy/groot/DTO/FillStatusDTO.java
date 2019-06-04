2d72434466d6fdb0757fb3a9171b6b0d793d078b
Gergely Kocsis <koger23@gmail.com>
2019-06-04 12:59:03 +0200

    feat(FillStatusDTO): create DTO to send status

---
+++ week-09/day-02/groot/src/main/java/com/greenfoxacademy/groot/DTO/FillStatusDTO.java
@@ -0,0 +1,49 @@
+package com.greenfoxacademy.groot.DTO;
+
+import com.greenfoxacademy.groot.model.Ship;
+
+public class FillStatusDTO {
+  private String received;
+  private long amount;
+  private String shipstatus;
+  private boolean ready;
+
+  public FillStatusDTO(Ship ship, String caliber, long amount) {
+    received = caliber;
+    this.amount = amount;
+    this.shipstatus = ship.getShipstatus();
+    this.ready = ship.isReady();
+  }
+
+  public String getReceived() {
+    return received;
+  }
+
+  public void setReceived(String received) {
+    this.received = received;
+  }
+
+  public long getAmount() {
+    return amount;
+  }
+
+  public void setAmount(long amount) {
+    this.amount = amount;
+  }
+
+  public String getShipstatus() {
+    return shipstatus;
+  }
+
+  public void setShipstatus(String shipstatus) {
+    this.shipstatus = shipstatus;
+  }
+
+  public boolean isReady() {
+    return ready;
+  }
+
+  public void setReady(boolean ready) {
+    this.ready = ready;
+  }
+}
