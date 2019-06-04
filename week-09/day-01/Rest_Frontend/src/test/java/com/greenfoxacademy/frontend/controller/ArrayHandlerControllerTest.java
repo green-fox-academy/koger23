092098aff22121fc5491f8665c35325ee0de75af
Gergely Kocsis <koger23@gmail.com>
2019-06-04 16:18:38 +0200

    feat(ArrayHandlerController): add test

---
+++ week-09/day-01/Rest_Frontend/src/test/java/com/greenfoxacademy/frontend/controller/ArrayHandlerControllerTest.java
@@ -0,0 +1,75 @@
+package com.greenfoxacademy.frontend.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.greenfoxacademy.frontend.model.dto.ArrayObject;
+import com.greenfoxacademy.frontend.service.ArrayHandlerSerivce;
+import com.greenfoxacademy.frontend.service.ILogService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.Charset;
+
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ArrayHandlerController.class)
+public class ArrayHandlerControllerTest {
+
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+          MediaType.APPLICATION_JSON.getSubtype(),
+          Charset.forName("utf8"));
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  ILogService logService;
+
+  @Test
+  public void handleArray_sumOK() throws Exception {
+    mockMvc.perform(post("/arrays")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"what\": \"sum\", " +
+                    "\"numbers\": [1, 2, 5, 10]}")
+                    )
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(contentType))
+            .andExpect(jsonPath("$.result", is(18)));
+  }
+
+  @Test
+  public void handleArray_multiplyOK() throws Exception {
+    mockMvc.perform(post("/arrays")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"what\": \"multiply\", " +
+                    "\"numbers\": [1, 2, 5, 10]}")
+                    )
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(contentType))
+            .andExpect(jsonPath("$.result", is(100)));
+  }
+
+  @Test
+  public void handleArray_doubleOK() throws Exception {
+    mockMvc.perform(post("/arrays")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"what\": \"double\", " +
+                    "\"numbers\": [1, 2, 5, 10]}")
+                    )
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(contentType))
+            .andExpect(content().json("{\"result\":[2, 4, 10, 20]}"));
+  }
+}
